6bcb8561f94b219e3423f6815eedde4198be8b83
Tim Heuett <tim.heuett@gmail.com>
2018-04-19 10:05:49 -0700

    tweaks bash install script

--- bash/install.sh
+++ bash/install.sh
@@ -12,13 +12,16 @@
 src_files='bashrc bash_aliases bash_profile bash_prompt'
 
 echo 'Backing up old files...'
-for f in $src_files; do mv -v ~/.$f ~/.${f}_backup; done
+mkdir ~/.bash_backup
+for f in $src_files; do mv -v ~/.$f ~/.bash_backup/$f; done
 
 echo 'Symlinking new files...'
-for f in $src_files; do ln -vs `pwd`/src/.$f ~/; done
-ln -vs `pwd`/src/bash_profile.d ~/.bash_profile.d
+for f in $src_files; do ln -vs `pwd`/$f ~/.$f; done
+# TODO if ~/.bash_profile.d already exists, this creates a symlink inside the dir
+ln -vs `pwd`/bash_profile.d ~/.bash_profile.d
 
 echo 'Sourcing new files...'
+
 source ~/.bash_profile
 
 echo 'Done!'
