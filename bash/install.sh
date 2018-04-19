9298d649592362283dc815dc51a4293cffdd4c63
Tim Heuett <tim.heuett@gmail.com>
2018-04-19 11:58:10 -0700

    adds bash config to install script

--- bash/install.sh
+++ bash/install.sh
@@ -9,19 +9,27 @@
 # .bash_prompt
 # .bash_profile.d/
 
-src_files='bashrc bash_aliases bash_profile bash_prompt'
+BASH_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sources='bashrc bash_aliases bash_profile bash_prompt bash_profile.d'
 
 echo 'Backing up old files...'
-mkdir ~/.bash_backup
-for f in $src_files; do mv -v ~/.$f ~/.bash_backup/$f; done
+backup_dir=${HOME}/.bash_backup
+mkdir $backup_dir
+for f in $sources; do
+  src=${HOME}/.$f
+  dest=${backup_dir}/$f
+  mv -v $src $dest
+done
 
 echo 'Symlinking new files...'
-for f in $src_files; do ln -vs `pwd`/$f ~/.$f; done
-# TODO if ~/.bash_profile.d already exists, this creates a symlink inside the dir
-ln -vs `pwd`/bash_profile.d ~/.bash_profile.d
+for f in $sources; do
+  src=${BASH_ROOT}/$f
+  dest=${HOME}/.$f
+  ln -vs $src $dest
+done
 
 echo 'Sourcing new files...'
-
 source ~/.bash_profile
 
 echo 'Done!'
--- install.sh
+++ install.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+DOT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # install brew and brew packages
 
-# install bash files
+########
+# BASH #
+########
+# creates symlinks in ~/. for:
+# .bashrc
+# .bash_profile
+# .bash_aliases
+# .bash_prompt
+# .bash_profile.d/
+cat <<EOF
+----------------------
+Installing bash config
+
+EOF
+source ${DOT_ROOT}/bash/install.sh
+
 # install misc (ctags, tmuxconf, etc)
 
 # install vimrc
