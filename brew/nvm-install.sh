b86e86774b0fbab1a011eed297e00aef6b4b7ee6
Tim Heuett <tim.heuett@gmail.com>
2018-04-25 08:34:34 -0700

    uses supported nvm install process (instead of homebrew)

---
+++ brew/nvm-install.sh
@@ -0,0 +1,3 @@
+mkdir $HOME/.npm
+# TODO: this adds some shit to ~/.bashrc that we don't want or need, so should handle that appropriately
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.10/install.sh | bash
