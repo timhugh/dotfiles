8ba29b219f5bd1657ce299942b268c486ef4f0e5
Tim Heuett <tim.heuett@gmail.com>
2018-05-22 10:04:31 -0700

    fix nvm warning (maybe)

--- bash/bash_profile.d/nvm.sh
+++ bash/bash_profile.d/nvm.sh
@@ -1,5 +1,4 @@
 # nvm init
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$(brew --prefix nvm)/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm use default
