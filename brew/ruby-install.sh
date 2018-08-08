5fc3a96179a3e05de2c70116c9837a6b0d4428ae
Tim Heuett <tim.heuett@gmail.com>
2018-08-08 13:06:18 -0700

    adds some nvim provider config stuff for ruby and python

--- brew/ruby-install.sh
+++ brew/ruby-install.sh
@@ -2,6 +2,8 @@
 ruby-install ruby-2.5.0
 gem install bundler
 
+gem install neovim
+
 gem install ripper-tags # ruby tag parser
 gem install gem-ctags # generates tags for gems on install
 gem ctags
