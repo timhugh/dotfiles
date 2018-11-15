79d9891c510ad68f970da94a8b792bc1a1f247dc
Tim Heuett <tim.heuett@gmail.com>
2018-11-14 23:33:25 -0700

    improves docker / docker machine commands for handling multiple machines

--- bash/bash_profile.d/docker.sh
+++ bash/bash_profile.d/docker.sh
@@ -3,7 +3,22 @@ alias dc='docker-compose'
 
 # docker-machine
 alias dm='docker-machine'
-alias dm-env='eval $(docker-machine env)'
+function dmc {
+  dmc-check-current && return
+  docker-machine $@ $CURRENT_DOCKER_MACHINE
+}
+function dmc-env {
+  dmc-check-current && return
+  eval $(docker-machine env $CURRENT_DOCKER_MACHINE)
+}
+function dmc-check-current {
+  [[ -z $CURRENT_DOCKER_MACHINE ]] && echo 'current docker machine is not set' && return 1
+}
+function dm-env {
+  [[ -z $1 ]] && echo 'Must specify name of docker machine' && return 1
+  export CURRENT_DOCKER_MACHINE=$1
+  dmc-env
+}
 
 # init docker env for default machine
-[[ "$(docker-machine status)" == *"Running"* ]] && eval "$(docker-machine env)"
+[[ -n $CURRENT_DOCKER_MACHINE ]] && [[ "$(docker-machine status $CURRENT_DOCKER_MACHINE)" == *"Running"* ]] && dmc-env
