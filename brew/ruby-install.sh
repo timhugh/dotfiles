55560d7de0f26e851d1503c1f8c816394843f8f0
Tim Heuett <tim.heuett@gmail.com>
2020-01-20 16:23:59 -0700

    add gem-browse to ruby gem list
    and clean up comments

--- brew/ruby-install.sh
+++ brew/ruby-install.sh
@@ -1,12 +1,10 @@
 # TODO: this stuff
-ruby-install ruby-2.5.0
+ruby-install ruby-2.6.3
 gem install bundler
 
 gem install neovim
 
-gem install ripper-tags # ruby tag parser
-gem install gem-ctags # generates tags for gems on install
-gem ctags
-# TODO: generate gem ctags using ripper-tags instead?
-
-gem install pronto pronto-rubocop
+gem install ripper-tags       # ruby tag parser
+gem install gem-ctags         # generates tags for gems on install
+gem ctags                     # TODO: generate gem ctags using ripper-tags instead?
+gem install gem-browse        # handy gem cloning/viewing/editing
