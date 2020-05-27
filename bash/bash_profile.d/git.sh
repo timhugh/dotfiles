be5bd340092be0d2294303e9d56e9696de9d64db
Tim Heuett <tim.heuett@gmail.com>
2020-05-27 14:50:30 -0700

    add git completion for git alias
    I don't know why it took me several years to google this but I'm glad I did

--- bash/bash_profile.d/git.sh
+++ bash/bash_profile.d/git.sh
@@ -1,5 +1,7 @@
-# handy git alias
+# handy git alias (now with autocomplete!)
 alias g='git'
+__git_complete g __git_main
+
 # use hub in place of git
 # (https://hub.github.com/)
 [[ $(command -v hub) ]] && alias git='hub'
