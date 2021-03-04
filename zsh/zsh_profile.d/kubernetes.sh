54bff275ac8a7188ea7c51d8a94d09dc662df79d
Tim Heuett <tim.heuett@gmail.com>
2021-03-04 08:17:45 -0800

    don't bother with kube aliases if it isn't installed

--- zsh/zsh_profile.d/kubernetes.sh
+++ zsh/zsh_profile.d/kubernetes.sh
@@ -1,8 +1,10 @@
-alias  k='kubectl'
-alias kt='kubetail'
-alias  h='helm'
+if [ command -v kubectl ]; then
+  alias  k='kubectl'
+  alias kt='kubetail'
+  alias  h='helm'
 
-alias kp='kubectl get pod'
-alias kd='kubectl get deployment'
-alias ks='kubectl get svc'
-alias ki='kubectl get ingress'
+  alias kp='kubectl get pod'
+  alias kd='kubectl get deployment'
+  alias ks='kubectl get svc'
+  alias ki='kubectl get ingress'
+fi
