6036cf5fec9abd76c7fb1d49814638f66d629194
Tim Heuett <tim.heuett@gmail.com>
2021-11-04 09:37:46 -0700

    support different platforms for go

--- install/golang.mk
+++ install/golang.mk
@@ -1,12 +1,13 @@
 DOT_ROOT ?= $(shell git rev-parse --show-toplevel)
 GO_VERSION ?= 1.16
+PLATFORM ?= linux-amd64
 
 .PHONY: golang
 golang: /usr/local/go
 
 include ${DOT_ROOT}/install/_common.mk
 
-/usr/local/go: ${DOT_ROOT}/tmp/go${GO_VERSION}.linux-amd64.tar.gz
+/usr/local/go: ${DOT_ROOT}/tmp/go${GO_VERSION}.${PLATFORM}.tar.gz
 	sudo tar -C /usr/local -xzvf $<
-${DOT_ROOT}/tmp/go${GO_VERSION}.linux-amd64.tar.gz: wget
-	wget -O $@ https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz
+${DOT_ROOT}/tmp/go${GO_VERSION}.${PLATFORM}.tar.gz: wget
+	wget -O $@ https://golang.org/dl/go${GO_VERSION}.${PLATFORM}.tar.gz
