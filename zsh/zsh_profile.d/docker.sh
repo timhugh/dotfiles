d3c0ae6d4b7b12d6488a5773ecfaf2166e322809
Tim Heuett <tim.heuett@gmail.com>
2022-01-29 13:36:12 -0800

    local docker service shortcuts
    add mx
    apparently safari doesn't like the host aliases

--- zsh/zsh_profile.d/docker.sh
+++ zsh/zsh_profile.d/docker.sh
@@ -1,8 +1,9 @@
 # handy aliases for things I tend to have running in docker
-alias     pg-dev='psql postgres://postgres:postgres@db/postgres'
-alias    rmq-dev='open http://rmq:15672'
-alias     es-dev='open http://elasticsearch:9200'
-alias kibana-dev='open http://kibana:5601'
+alias     pg-dev='psql postgres://postgres:postgres@localhost:5432/postgres'
+alias    rmq-dev='open http://localhost:15672'
+alias     mx-dev='open http://localhost:1080'
+alias     es-dev='open http://localhost:9200'
+alias kibana-dev='open http://localhost:5601'
 
 alias dc="docker-compose"
 alias dcl="docker-compose logs -f --tail 0"
