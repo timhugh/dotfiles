190305b358c3dfbf91e9367467e67d612f698f68
Tim Heuett <tim.heuett@gmail.com>
2022-02-16 17:45:27 -0800

    add heroku autocomplete to zshrc

---
+++ zsh/zsh_profile.d/heroku.sh
@@ -0,0 +1,3 @@
+# heroku autocomplete setup
+HEROKU_AC_ZSH_SETUP_PATH=/Users/tim/Library/Caches/heroku/autocomplete/zsh_setup && test -f $HEROKU_AC_ZSH_SETUP_PATH && source $HEROKU_AC_ZSH_SETUP_PATH;
+
