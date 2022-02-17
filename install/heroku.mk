3cc95db2416fe477a793f479d2ec69702ae561e7
Tim Heuett <tim.heuett@gmail.com>
2022-02-16 17:45:12 -0800

    add placeholder for heroku makefile

---
+++ install/heroku.mk
@@ -0,0 +1,11 @@
+# placeholder
+
+# install heroku CLI
+# homebrew on mac, apt on debian
+
+# install plugins:
+# heroku plugins:install heroku-builds
+# heroku plugins:install heroku-accounts
+
+# create personal account?
+
