a15be0fadaf17a356906ceb2808d7d9ef04fb39c
Tim Heuett <tim.heuett@gmail.com>
2022-05-02 04:52:07 -0700

    bump ruby, ruby-install versions
    removed wget as a requirement b/c it'll be there and then there's no apt dependency for macos

--- install/ruby.mk
+++ install/ruby.mk
@@ -1,10 +1,10 @@
 DOT_ROOT ?= $(shell git rev-parse --show-toplevel)
-RUBY_VERSION ?= 2.7.2
+RUBY_VERSION ?= 2.7.5
 CHRUBY_VERSION ?= 0.3.9
-RUBY_INSTALL_VERSION ?= 0.7.1
+RUBY_INSTALL_VERSION ?= 0.8.3
 
 .PHONY: ruby
-ruby: ruby-${RUBY_VERSION} chruby ${HOME}/.ruby-version 
+ruby: ruby-${RUBY_VERSION} chruby ${HOME}/.ruby-version
 
 include ${DOT_ROOT}/install/_common.mk
 
@@ -20,7 +20,7 @@ chruby: /usr/local/bin/chruby-exec
 	sudo $(MAKE) -C ${DOT_ROOT}/tmp/chruby-${CHRUBY_VERSION} install
 ${DOT_ROOT}/tmp/chruby-${CHRUBY_VERSION}: ${DOT_ROOT}/tmp/chruby-${CHRUBY_VERSION}.tar.gz
 	tar -xzvf $< -C ${DOT_ROOT}/tmp
-${DOT_ROOT}/tmp/chruby-${CHRUBY_VERSION}.tar.gz: wget
+${DOT_ROOT}/tmp/chruby-${CHRUBY_VERSION}.tar.gz:
 	mkdir -p ${DOT_ROOT}/tmp
 	test -f $@ || wget -O $@ https://github.com/postmodern/chruby/archive/v${CHRUBY_VERSION}.tar.gz
 
@@ -30,7 +30,7 @@ ruby-install: /usr/local/bin/ruby-install
 	sudo $(MAKE) -C ${DOT_ROOT}/tmp/ruby-install-${RUBY_INSTALL_VERSION} install
 ${DOT_ROOT}/tmp/ruby-install-${RUBY_INSTALL_VERSION}: ${DOT_ROOT}/tmp/ruby-install-${RUBY_INSTALL_VERSION}.tar.gz
 	tar -xzvf $< -C ${DOT_ROOT}/tmp
-${DOT_ROOT}/tmp/ruby-install-${RUBY_INSTALL_VERSION}.tar.gz: wget
+${DOT_ROOT}/tmp/ruby-install-${RUBY_INSTALL_VERSION}.tar.gz:
 	mkdir -p ${DOT_ROOT}/tmp
 	test -f $@ || wget -O $@ https://github.com/postmodern/ruby-install/archive/v${RUBY_INSTALL_VERSION}.tar.gz
 
