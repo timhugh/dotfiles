d27f6d635569c4e19afad8e363f157ef7648a48a
Tim Heuett <tim.heuett@gmail.com>
2024-06-03 16:38:26 -0700

    set dock settings

--- dock/install.sh
+++ dock/install.sh
@@ -4,3 +4,7 @@
 /opt/homebrew/bin/dockutil --add ${HOME} --display folder --view grid
 /opt/homebrew/bin/dockutil --add ${HOME}/Downloads --display folder --view grid
 
+defaults write com.apple.dock show-recents -bool "false"
+defaults write com.apple.dock minimize-to-application -bool "true"
+killall Dock
+
