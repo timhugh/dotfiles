665b1bbb0a343648755912b7818d82490fdd1171
Tim Heuett <tim.heuett@gmail.com>
2024-06-05 17:25:37 -0700

    add some lsp keymaps
    shamelessy lifted from https://github.com/helderberto/dotfiles/blob/main/nvim/.config/nvim/lua/plugins/lsp.lua

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -9,4 +9,9 @@ vim.keymap.set('n', '<c-j>', '<c-w>j')
 vim.keymap.set('n', '<c-k>', '<c-w>k')
 vim.keymap.set('n', '<c-l>', '<c-w>l')
 
+-- clear highlights after searching
+vim.keymap.set('n', '<leader>/', '<cmd>:nohlsearch<cr>')
+
+-- TODO: this should only be enabled in c/c++ files
 vim.keymap.set('n', '<leader>h', '<cmd>:ClangdSwitchSourceHeader<cr>')
+
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,42 +1,71 @@
 return {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    dependencies = {
-        'neovim/nvim-lspconfig',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/nvim-cmp',
-        'L3MON4D3/LuaSnip',
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        dependencies = {
+            'neovim/nvim-lspconfig',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/nvim-cmp',
+            'L3MON4D3/LuaSnip',
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+        },
+        config = function()
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_lspconfig()
+
+            lsp_zero.on_attach(function(_, buffer)
+                lsp_zero.default_keymaps({ buffer = buffer })
+            end)
+
+            lsp_zero.configure('lua_ls', {
+                settings = {
+                    diagnostics = {
+                        globals = { 'vim' },
+                    },
+                },
+            })
+
+            lsp_zero.setup()
+
+            lsp_zero.on_attach(function(_, buffer)
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {
+                    buffer = buffer,
+                    desc = "Go to definition",
+                })
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {
+                    buffer = buffer,
+                    desc = "Go to declaration",
+                })
+                vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, {
+                    buffer = buffer,
+                    desc = "Go to implementation",
+                })
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, {
+                    buffer = buffer,
+                    desc = "Go to references",
+                })
+            end)
+
+            local mason = require("mason")
+            local mason_lspconfig = require("mason-lspconfig")
+
+            mason.setup()
+
+            mason_lspconfig.setup({
+                ensure_installed = {
+                    "tsserver",
+                    "html",
+                    "cssls",
+                    "tailwindcss",
+                    "lua_ls",
+                    "gopls",
+                    "clangd",
+                },
+                handlers = {
+                    lsp_zero.default_setup,
+                },
+            })
+        end
     },
-    config = function()
-        local lsp_zero = require('lsp-zero')
-        lsp_zero.extend_lspconfig()
-
-        lsp_zero.on_attach(function(_, buffer)
-            lsp_zero.default_keymaps({ buffer = buffer })
-        end)
-
-        local mason = require("mason")
-        local mason_lspconfig = require("mason-lspconfig")
-
-        mason.setup()
-
-        mason_lspconfig.setup({
-            ensure_installed = {
-                "tsserver",
-                "html",
-                "cssls",
-                "tailwindcss",
-                "lua_ls",
-                "gopls",
-                "clangd",
-            },
-            handlers = {
-                function(server_name)
-                    require('lspconfig')[server_name].setup({})
-                end,
-            },
-        })
-    end
 }
