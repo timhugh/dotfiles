0fb8c0f2599216ad99feb35f2cc226aedc290150
Tim Heuett <tim.heuett@gmail.com>
2024-06-12 10:20:54 -0700

    consolidate os script

--- base/os/install.sh
+++ base/os/install.sh
@@ -4,8 +4,29 @@ set -e
 
 echo "installing base/os subpackage"
 
-"${DOT_ROOT}/base/os/dock.sh"
-"${DOT_ROOT}/base/os/misc.sh"
+echo "configuring dock icons"
+/opt/homebrew/bin/dockutil --remove all
+/opt/homebrew/bin/dockutil --add "${HOME}" --display folder --view grid
+/opt/homebrew/bin/dockutil --add "${HOME}/Downloads" --display folder --view grid
+
+echo "configuring dock settings"
+defaults write com.apple.dock show-recents -bool "false"
+defaults write com.apple.dock minimize-to-application -bool "true"
+
+# screenshot tool
+echo "configuring screenshot tool"
+defaults write com.apple.screencapture location -string "~/Downloads"
+defaults write com.apple.screencapture show-thumbnail -bool "false"
+
+# 24 hr time
+echo "configuring menu bar clock"
+defaults write com.apple.menuextra.clock "DateFormat" -string "\"EEE MMM d HH:mm\""
+
+echo "restarting dock"
+killall Dock
+
+echo "restarting system ui server"
+killall SystemUIServer
 
 echo "base/os subpackage complete"
 
