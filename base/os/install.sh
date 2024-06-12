91260c57490ecd5e5e1fb9f573c26afca51f1991
Tim Heuett <tim.heuett@gmail.com>
2024-06-12 10:35:57 -0700

    add config change for spaces rearranging

--- base/os/install.sh
+++ base/os/install.sh
@@ -5,7 +5,9 @@ set -e
 echo "installing base/os subpackage"
 
 echo "configuring dock icons"
+# clear all of that starter garbage from the dock
 /opt/homebrew/bin/dockutil --remove all
+# add home and downloads folders
 /opt/homebrew/bin/dockutil --add "${HOME}" --display folder --view grid
 /opt/homebrew/bin/dockutil --add "${HOME}/Downloads" --display folder --view grid
 
@@ -13,7 +15,10 @@ echo "configuring dock settings"
 defaults write com.apple.dock show-recents -bool "false"
 defaults write com.apple.dock minimize-to-application -bool "true"
 
-# screenshot tool
+echo "configuring mission control"
+# don't automatically rearrange spaces
+defaults write com.apple.dock mru-spaces -bool "false"
+
 echo "configuring screenshot tool"
 defaults write com.apple.screencapture location -string "~/Downloads"
 defaults write com.apple.screencapture show-thumbnail -bool "false"
