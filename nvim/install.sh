c8eef14934b2c3ba3387a70000b9f49608d0ca19
Tim Heuett <tim.heuett@gmail.com>
2024-06-12 12:17:32 -0700

    link nvim config during install

--- nvim/install.sh
+++ nvim/install.sh
@@ -7,5 +7,8 @@ echo "installing nvim package"
 echo "bundling brewfile"
 /opt/homebrew/bin/brew bundle --file "${DOT_ROOT}/nvim/Brewfile"
 
+echo "linking configuration"
+ln -s "${DOT_ROOT}/nvim" "${HOME}/.config/nvim"
+
 echo "nvim package complete"
 
