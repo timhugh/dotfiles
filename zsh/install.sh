20246201e60ea9dc802a69899cedc22757f1aacf
Tim Heuett <tim.heuett@gmail.com>
2024-06-12 11:23:15 -0700

    fix quoting in zsh install

--- zsh/install.sh
+++ zsh/install.sh
@@ -16,9 +16,9 @@ fi
 
 # link configuration files
 echo "linking zsh configuration files"
-test -f "${HOME}/.zshrc" || ln -s "${DOT_ROOT}/zsh/zshrc ${HOME}/.zshrc"
-test -f "${HOME}/.zsh_aliases" || ln -s "${DOT_ROOT}/zsh/zsh_aliases ${HOME}/.zsh_aliases"
-test -d "${HOME}/.zsh_profile.d" || ln -s "${DOT_ROOT}/zsh/zsh_profile.d ${HOME}/.zsh_profile.d"
+test -f "${HOME}/.zshrc" || ln -s "${DOT_ROOT}/zsh/zshrc" "${HOME}/.zshrc"
+test -f "${HOME}/.zsh_aliases" || ln -s "${DOT_ROOT}/zsh/zsh_aliases" "${HOME}/.zsh_aliases"
+test -d "${HOME}/.zsh_profile.d" || ln -s "${DOT_ROOT}/zsh/zsh_profile.d" "${HOME}/.zsh_profile.d"
 
 echo "zsh package complete"
 
