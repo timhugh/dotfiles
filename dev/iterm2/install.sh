1b9479b0e84962d332095a1cd6ecc02f2f5c7a37
Tim Heuett <tim.heuett@gmail.com>
2024-06-12 11:01:46 -0700

    add meslo font to dev package

--- dev/iterm2/install.sh
+++ dev/iterm2/install.sh
@@ -9,5 +9,9 @@ echo "configuring iterm2 to use dotfiles configuration at ${DOT_ROOT}/dev/iterm2
 defaults write com.googlecode.iterm2 PrefsCustomFolder -string "${DOT_ROOT}/dev/iterm2"
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
 
-echo "dev/iterm2 subpackage complete"
+echo "installing meslo nerd font"
+wget -O "${DOT_ROOT}/tmp/Meslo.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip"
+unzip -D "${DOT_ROOT}/tmp/Meslo" "${DOT_ROOT}/tmp/Meslo.zip"
+cp "${DOT_ROOT}/tmp/Meslo/*.ttf" "${HOME}/Library/Fonts"
 
+echo "dev/iterm2 subpackage complete"
