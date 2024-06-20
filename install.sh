858ab6a3200099d13c35cbc95d85b341dd43dc6e
Tim Heuett <tim.heuett@gmail.com>
2024-06-20 16:33:20 -0700

    scaffold install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+
+# if dotfiles repo exists
+#   git update repo
+# else
+#   Download dotfiles repo to ${HOME}/.dotfiles
+
+# Link to ${HOME}/git/dotfiles if not already linked
+
+# if packages provided in ENV
+#   run install with packages
+# else
+#   run install with default packages
+
+# add git remote to dotfiles repo
+
