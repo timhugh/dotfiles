4537d050d4b295ac7f7c2eac0fb4ca133b187a05
Tim Heuett <tim.heuett@gmail.com>
2024-06-21 12:11:36 -0700

    port nvim config changes from old branch

--- packages/base/nvim-config/lua/plugins/treesitter.lua
+++ packages/base/nvim-config/lua/plugins/treesitter.lua
@@ -1,9 +1,52 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    config = {
-        ensure_installed = {
-            "all",
-        },
-    }
+    config = function()
+        require('nvim-treesitter.configs').setup({
+            ensure_installed = {
+                "c",
+                "cpp",
+                "css",
+                "dockerfile",
+                "git_config",
+                "git_rebase",
+                "gitattributes",
+                "gitcommit",
+                "gitignore",
+                "go",
+                "gomod",
+                "gosum",
+                "gotmpl",
+                "gowork",
+                "graphql",
+                "helm",
+                "html",
+                "java",
+                "javascript",
+                "jq",
+                "json",
+                "kotlin",
+                "ledger",
+                "llvm",
+                "lua",
+                "make",
+                "markdown",
+                "markdown_inline",
+                "regex",
+                "ruby",
+                "scss",
+                "sql",
+                "ssh_config",
+                "terraform",
+                "toml",
+                "tsv",
+                "tsx",
+                "typescript",
+                "vim",
+                "vue",
+                "xml",
+                "yaml",
+            }
+        })
+    end
 }
 
