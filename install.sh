01f9e4216244a68fa75b4bb7208cfb3b4529809b
Tim Heuett <tim.heuett@gmail.com>
2024-06-21 17:29:10 -0700

    your friendly neighborhood installer

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-set -ex
+set -e
 setopt nullglob
 
 git_repo="git@github.com:timhugh/dotfiles.git"
@@ -8,84 +8,110 @@ branch=main
 dotfiles_src="https://github.com/timhugh/dotfiles/archive/${branch}.zip"
 root="${HOME}/share/dotfiles"
 
+# save stdout descriptor
+exec 3>&1
+# redirect everything else to a file
+mkdir -p "$root"/tmp
+timestamp=$(date +%Y%m%d%H%M%S)
+logfile="$root/tmp/install-$timestamp.log"
+exec > "$logfile" 2>&1
+
+print() {
+    echo "$@"
+    echo "$@" >&3
+}
+
 function install_xcode_tools() {
     if xcode-select -p &> /dev/null
     then
-        echo "Xcode tools already installed"
+        print "I see you already have Xcode Command Line Tools installed."
         return;
     fi
 
-    echo "Installing Xcode Command Line Tools"
+    print "Let's install Xcode Command Line Tools..."
     # touch this file so softwareupdate will anticipate an xcode install
     touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
     # get the version of the latest xcode tools and install
     VER="$(softwareupdate --list | grep "\*.*Command Line" | tail -n 1 | sed 's/^[^C]* //')"
     softwareupdate --install "$VER"
-    echo "Xcode Command Line Tools installed successfully"
+    print "Okay, now you have a C compiler."
 }
 
 function install_homebrew() {
     if command -v /opt/homebrew/bin/brew &> /dev/null
     then
-        echo "Homebrew already installed"
+        print "Nice! You already have Homebrew installed."
         return
     fi
 
-    echo "Installing Homebrew"
+    print "Let's install Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "Homebrew installed successfully"
+    print "Great. Now you can install all the things!"
 }
 
 function install_rosetta() {
     if [[ "$(uname -m)" != "arm64" ]]; then
-        echo "Rosetta install not needed"
+        print "You're on an Intel Mac, no need to install Rosetta."
         return
     fi
 
     # TODO: this is not working; it's installing anyway
     if ! arch -arch x86_64 uname -m &> /dev/null
     then
-        echo "Rosetta already installed"
+        print "You already have Rosetta installed."
         return
     fi
 
-    echo "Installing Rosetta"
+    print "Let's install Rosetta..."
     /usr/sbin/softwareupdate --install-rosetta --agree-to-license
-    echo "Rosetta installed successfully"
+    print "All done! Now you can run Intel apps on your arm64 Mac."
 }
 
-echo "Welcome to your friendly dotfiles installer!"
-echo "Starting with the basics..."
+print "Welcome to your friendly dotfiles installer!"
+print "I'm going to do most of this quietly, but if you want to see the nitty gritty, check out this logfile:"
+print "$logfile"
 echo
+echo "Hey look at you, log sniffing!"
+print
+print "We'll start with the basics..."
 
+print "---"
 if [[ -d "$root" ]]; then
-    echo "Dotfiles repo already exists at $root"
+    print "It looks like you've already got the dotfiles repo at $root"
 else
-    echo "Downloading dotfiles..."
+    print "First, we'll download a zip of the dotfiles repo and extract it to $root"
     mkdir -p "$HOME"/share
     curl -L -o "$root".zip "$dotfiles_src"
     unzip "$root".zip -d "$HOME"/share
     rm "$root".zip
     mv "${HOME}/share/dotfiles-${branch}" "$HOME"/share/dotfiles
+    print "That's done"
 fi
 
 if [[ -e "${HOME}"/.dotfiles ]] && [[ $(readlink -f "${HOME}"/.dotfiles) != "$root" ]]; then
-    echo "Dotfiles symlink exists at ${HOME}/.dotfiles but points to $(readlink -f "${HOME}"/.dotfiles)"
-    echo "Please remove the existing symlink and re-run this script:"
-    echo "  rm ${HOME}/.dotfiles"
+    print "Uh oh!"
+    print "You've already got a symlink at ${HOME}/.dotfiles, but it doesn't point to $root"
+    print "You'll have to remove that symlink and re-run this script:"
+    print "  rm ${HOME}/.dotfiles"
+    print
+    print "I'll wait here"
     exit 1
 elif [[ -e "${HOME}/.dotfiles" ]]; then
     echo "Dotfiles symlink already exists at ${HOME}/.dotfiles"
 else
-    echo "Linking to ${HOME}/.dotfiles"
+    print "Okay now I'm going to link it to ${HOME}/.dotfiles"
     ln -fs "$HOME"/share/dotfiles "$HOME"/.dotfiles
 fi
 
 cd "$root"
 
+print "---"
 install_xcode_tools
+print "---"
 install_homebrew
+print "---"
 install_rosetta
+print "---"
 
 # Determine the set of packages to install
 # (required_packages will always be installed)
@@ -96,7 +122,10 @@ if [[ $(uname) == "Darwin" ]]; then
     default_packages+=(macos node ruby go)
 fi
 if [[ -z "$*" ]]; then
-    echo "No packages passed to install script. Installing the default set."
+    print "You didn't specify any packages, so I'll use the default set"
+    print "If you want to install a different set, you can pass them as arguments to this script, like: "
+    print "  ./install.sh 3dprinting gamedev"
+    print
     packages+=("${required_packages[@]}")
     packages+=("${default_packages[@]}")
 else
@@ -105,20 +134,24 @@ else
 fi
 
 # confirm installation
-echo "Install packages?"
+print "Here's what I'm going to install:"
 for package in "${packages[@]}"; do
-    echo "  - $package"
+    print "  - $package"
 done
-echo "(y/N) > "
+print "Sound good? (y/N) > "
 read -rk 1 reply
 echo
 [[ $reply == "y" ]] || exit 1
 
+echo "It didn't print in this log, but you said yes!"
+
 mkdir -p "${HOME}/.zsh_profile.d"
 
+print
+print "---"
 # install packages
 for package in "${packages[@]}"; do
-    echo "Installing $package"
+    print "Installing $package"
 
     cd "$root/packages/$package"
 
@@ -163,10 +196,15 @@ for package in "${packages[@]}"; do
     echo "Done installing $package"
 done
 
-echo "Configuring git remote in dotfiles repo"
+print "---"
+print "Okay, everything is installed!"
+
+print "Last thing: now that we have git, let's configure the dotfiles repo to point at the remote"
 cd "$root"
 [[ -d .git ]] || git init
 [[ $(git remote get-url origin) == $git_repo ]] || git remote add origin $git_repo
 
-echo "Dotfiles installed successfully"
+print
+print "All done! Enjoy your new dotfiles!"
+print "Come see me again if you want more packages!"
 
