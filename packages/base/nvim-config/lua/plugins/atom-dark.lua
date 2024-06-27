212a035a18188a711bc09e7137e041e429ec47cb
Tim Heuett <tim.heuett@gmail.com>
2024-06-27 15:13:14 -0700

    nvim: auto toggle theme for dark/light mode

--- packages/base/nvim-config/lua/plugins/atom-dark.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "gosukiwi/vim-atom-dark",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd([[colorscheme atom-dark]])
-    end,
-}
-
---
+++ packages/base/nvim-config/lua/plugins/theme.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "f-person/auto-dark-mode.nvim",
+        dependencies = {
+
+            "gosukiwi/vim-atom-dark",
+            "rmehri01/onenord.nvim",
+        },
+        opts = {
+            update_interval = 1000,
+            set_dark_mode = function()
+                vim.api.nvim_set_option("background", "dark")
+                vim.cmd("colorscheme onenord")
+            end,
+            set_light_mode = function()
+                vim.api.nvim_set_option("background", "light")
+                vim.cmd("colorscheme onenord-light")
+            end,
+        },
+    },
+}
