ada96114c84fbe3e439ae265db11f7131b223df0
Tim Heuett <tim.heuett@gmail.com>
2024-06-26 21:09:07 -0700

    cross-platform tweaks to zsh profile

--- packages/ruby/ruby.zsh
+++ packages/ruby/ruby.zsh
@@ -1,5 +1,7 @@
-source "$(brew --prefix chruby)"/share/chruby/chruby.sh
-source "$(brew --prefix chruby)"/share/chruby/auto.sh
+# TODO: this is usually correct, but it's not for sure
+prefix=/usr/local
+source "${prefix}"/share/chruby/chruby.sh
+source "${prefix}"/share/chruby/auto.sh
 
 alias be="bundle exec"
 
