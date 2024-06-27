5aeadc3d9e79022add2e2f409e7c8eead0438f2b
Tim Heuett <tim.heuett@gmail.com>
2024-06-27 15:51:31 -0700

    nvim: add more keymaps for telescope

--- packages/base/nvim-config/lua/plugins/telescope.lua
+++ packages/base/nvim-config/lua/plugins/telescope.lua
@@ -1,3 +1,4 @@
+-- TODO: https://github.com/nvim-telescope/telescope-frecency.nvim
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
@@ -6,10 +7,23 @@ return {
     },
     init = function()
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-        vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find file" })
+        vim.keymap.set("n", "<leader>fla", builtin.current_buffer_fuzzy_find, { desc = "Find text in current buffer" })
+        vim.keymap.set("n", "<leader>fG", builtin.live_grep, { desc = "Grep all files" })
+        vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffer" })
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help page"} )
+        vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "Find in quickfix list" })
+
+        vim.keymap.set("n", "<leader>fgs", builtin.git_status, { desc = "Find in git modified files" })
+        vim.keymap.set("n", "<leader>fgb", builtin.git_branches, { desc = "Find git branch" })
+
+        vim.keymap.set("n", "<leader>ft", builtin.treesitter, { desc = "Find symbols in current buffer (Treesitter)" })
+
+        vim.keymap.set("n", "<leader>flr", builtin.lsp_references, { desc = "Find references (LSP)" })
+        vim.keymap.set("n", "<leader>fli", builtin.lsp_incoming_calls, { desc = "Find incoming calls (LSP)" })
+        vim.keymap.set("n", "<leader>flo", builtin.lsp_outgoing_calls, { desc = "Find outgoing calls (LSP)" })
+        vim.keymap.set("n", "<leader>fls", builtin.lsp_document_symbols, { desc = "Find symbols in current buffer (LSP)" })
+        vim.keymap.set("n", "<leader>flS", builtin.lsp_workspace_symbols, { desc = "Find symbols in workspace (LSP)" })
     end,
 }
 
