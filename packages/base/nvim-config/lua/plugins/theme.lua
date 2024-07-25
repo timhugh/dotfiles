57e86a009c93e0d9e833bc58c7e454b9268bb015
Tim Heuett <tim.heuett@gmail.com>
2024-07-24 18:42:14 -0700

    switch nvim theme

--- packages/base/nvim-config/lua/plugins/theme.lua
+++ packages/base/nvim-config/lua/plugins/theme.lua
@@ -2,19 +2,19 @@ return {
     {
         "f-person/auto-dark-mode.nvim",
         dependencies = {
-
             "gosukiwi/vim-atom-dark",
             "rmehri01/onenord.nvim",
+            "EdenEast/nightfox.nvim"
         },
         opts = {
             update_interval = 1000,
             set_dark_mode = function()
                 vim.api.nvim_set_option("background", "dark")
-                vim.cmd("colorscheme onenord")
+                vim.cmd("colorscheme carbonfox")
             end,
             set_light_mode = function()
                 vim.api.nvim_set_option("background", "light")
-                vim.cmd("colorscheme onenord-light")
+                vim.cmd("colorscheme dayfox")
             end,
         },
     },
