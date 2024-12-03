91b9c522c9b04104679c8c8ca1ce9adf17c7b259
Tim Heuett <tim.heuett@gmail.com>
2024-12-02 17:53:00 -0800

    nvim: add good old vim-fugitive and gitsigns

--- packages/base/nvim-config/lua/plugins/git.lua
+++ packages/base/nvim-config/lua/plugins/git.lua
@@ -1,11 +1,22 @@
 return {
-    { "airblade/vim-gitgutter" },
-    {
-        "FabijanZulj/blame.nvim",
-        config = function()
-            require("blame").setup()
+  {
+    "tpope/vim-fugitive",
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
 
-            vim.api.nvim_set_keymap("n", "<leader>gb", "<cmd>BlameToggle<cr>", { noremap = true, silent = true })
-        end
-    }
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<cr>", { desc = "Preview current hunk" })
+    end
+  },
+  {
+    "FabijanZulj/blame.nvim",
+    keys = {
+      { "<leader>gb", "<cmd>BlameToggle<cr>", desc = "Toggle git blame" }
+    },
+    config = function()
+      require("blame").setup()
+    end
+  }
 }
