470c01a82fb17982bc4de014867374a4a20a59cd
Tim Heuett <tim.heuett@gmail.com>
2024-12-16 15:56:42 -0800

    nvim: add keybinds for navigating git hunks

--- packages/base/nvim-config/lua/plugins/git.lua
+++ packages/base/nvim-config/lua/plugins/git.lua
@@ -8,6 +8,8 @@ return {
       require("gitsigns").setup()
 
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<cr>", { desc = "Preview current hunk" })
+      vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk<cr>", { desc = "Jump to next hunk" })
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns prev_hunk<cr>", { desc = "Jump to previous hunk" })
     end
   },
   {
