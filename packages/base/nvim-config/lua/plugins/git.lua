2f5d8a65e8c6f6e09637ca6ac2ec59758c265d43
Tim Heuett <tim.heuett@gmail.com>
2024-12-18 15:14:44 -0800

    nvim: add more gitsigns keybinds

--- packages/base/nvim-config/lua/plugins/git.lua
+++ packages/base/nvim-config/lua/plugins/git.lua
@@ -10,6 +10,10 @@ return {
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<cr>", { desc = "Preview current hunk" })
       vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk<cr>", { desc = "Jump to next hunk" })
       vim.keymap.set("n", "<leader>gp", ":Gitsigns prev_hunk<cr>", { desc = "Jump to previous hunk" })
+      vim.keymap.set("n", "<leader>gR", ":Gitsigns reset_hunk<cr>", { desc = "Reset current hunk" })
+      vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk<cr>", { desc = "Stage current hunk" })
+      vim.keymap.set("n", "<leader>gu", ":Gitsigns undo_stage_hunk<cr>", { desc = "Undo stage current hunk" })
+      vim.keymap.set("n", "<leader>gd", ":Gitsigns diffthis<cr>", { desc = "Diff current file" })
     end
   },
   {
