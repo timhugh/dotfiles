7d4a46a97b3abc65c3f8b4db10b81d733dbdc88e
Tim Heuett <tim.heuett@gmail.com>
2024-12-21 08:26:22 -0800

    nvim: add key for additional terminal

--- packages/base/nvim-config/lua/plugins/toggleterm.lua
+++ packages/base/nvim-config/lua/plugins/toggleterm.lua
@@ -1,13 +1,15 @@
 return {
   "akinsho/toggleterm.nvim",
   keys = {
-    { "<leader>T", "<cmd>ToggleTerm direction=horizontal size=20<cr>", desc = "Open toggleterm" },
+    { "<leader>T", "<cmd>ToggleTerm<cr>", desc = "Open toggleterm" },
+    { "<leader>TT", "<cmd>2ToggleTerm<cr>", desc = "Open second toggleterm" },
   },
   config = function()
     require("toggleterm").setup({
       open_mapping = [[<c-\>]],
-      direction = "float",
+      direction = "horizontal",
       start_in_insert = true,
+      size = 20,
     })
 
     vim.api.nvim_create_user_command("X", ":TermExec direction=horizontal size=20 cmd=<q-args><cr>", { nargs = 1 })
