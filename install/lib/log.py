686123a545dba835a0a5ea68f392b881ba46160a
Tim Heuett <tim.heuett@gmail.com>
2024-12-23 07:32:42 -0800

    install: impl xcode, brew installers

--- install/lib/log.py
+++ install/lib/log.py
@@ -1,9 +1,14 @@
+# vim:fileencoding=utf-8
+
 import datetime
 import os
 import sys
 from contextlib import contextmanager
+from typing import List
+import subprocess
 
 __LOG_DIR__ = os.path.join(os.getenv("HOME"), "tmp", "dotfiles")
+__DEBUG__ = os.getenv("DEBUG", "false") == "true"
 
 __ANSI_UP__ = "\033[A"
 __ANSI_DOWN__ = "\033[B"
@@ -14,15 +19,19 @@ __SUCCESS__ = "✅"
 __FAILURE__ = "❌"
 __SKIP__ = "⏭️"
 
+__HEADER_LINE__ = "#" * 80
+
 
 def __generate_log_file_path__():
-    return "test.log"
-    # return os.path.join(
-    #         __LOG_DIR__,
-    #         "install-" +
-    #         datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S") +
-    #         ".log"
-    #         )
+    if __DEBUG__:
+        return "test.log"
+    else:
+        return os.path.join(
+                __LOG_DIR__,
+                "install-" +
+                datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S") +
+                ".log"
+                )
 
 
 __log_file_path__ = os.getenv(
@@ -52,27 +61,33 @@ class Task:
         self.__file = file
         self.__num_lines = 0
 
-        self.log_to_file(f"### {name} started")
+        self.log_to_file(f"{__HEADER_LINE__}\n{name}\n{__HEADER_LINE__}")
         self.log_to_screen(f"{__IN_PROGRESS__} {name}")
 
     def log_to_screen(self, msg: str):
-        print(msg)
+        print(msg, flush=True)
         self.__num_lines += 1
 
     def log_to_file(self, msg: str):
-        print(msg, file=self.__file)
+        print(msg, file=self.__file, flush=True)
 
     def clear_line(self):
         sys.stdout.write(__ANSI_UP__ + __ANSI_CLEAR_LINE__)
 
-    def step(self, message: str, callback: callable = None) -> any:
+    def command(self, message: str, command: List[str], ignore_errors: bool = False):
         try:
             self.log_to_screen(f"\t{__IN_PROGRESS__} {message}")
-            self.log_to_file(f"\t{message}")
+            self.log_to_file(f"## {message}")
 
             result = None
-            if callback:
-                result = callback()
+            if command:
+                result = subprocess.run(command, capture_output=True)
+                self.log_to_file(result.stdout)
+                self.log_to_file(result.stderr)
+                if result.returncode != 0 and not ignore_errors:
+                    self.log_to_screen(result.stdout.decode("utf-8"))
+                    self.log_to_screen(result.stderr.decode("utf-8"))
+                    raise Exception("Command failed with non-zero exit code")
 
             self.clear_line()
             self.log_to_screen(f"\t{__SUCCESS__} {message}")
@@ -82,11 +97,14 @@ class Task:
             self.log_to_screen(f"\t{__FAILURE__} {message}")
             raise e
 
+    def msg(self, message: str):
+        self.log_to_screen(f"\t{message}")
+        self.log_to_file(f"## {message}")
+
     def finish(self):
         for n in range(self.__num_lines + 1):
             self.clear_line()
         self.log_to_screen(f"{__SUCCESS__} {self.__name}")
-        self.log_to_file(f"### {self.__name} finished")
 
     def fail(self, exception: Exception):
         for n in range(self.__num_lines):
@@ -95,5 +113,6 @@ class Task:
         for n in range(self.__num_lines):
             sys.stdout.write(__ANSI_DOWN__)
         self.log_to_screen(f"Error: {exception}")
+
         self.log_to_file(f"### {self.__name} failed")
         self.log_to_file(f"Error: {exception}")
--- install/xcode.py
+++ install/xcode.py
@@ -2,43 +2,26 @@
 
 import os
 import sys
-import subprocess
 
 file_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(file_dir, "lib"))
 import log
 
 
-def is_installed() -> bool:
-    return subprocess.run(["xcode-select", "-p"], check=False, capture_output=True).returncode == 0
-
-
-def trigger_install() -> None:
-    subprocess.run(["touch", "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"], capture_output=True, check=True)
-
-
-def get_latest_version() -> str:
-    version = None
-    for line in subprocess.run(["softwareupdate", "--list"], capture_output=True, check=True).stdout.decode("utf-8").split("\n"):
-        if "Command Line Tools" in line:
-            version = line.split("\t")[-1]
-            break
-    return version
-
-
-def install_version(version: str) -> None:
-    subprocess.run(["softwareupdate", "--install", version], capture_output=True, check=True)
-
-
 def install_xcode():
     with log.task("Xcode Command Line Tools") as task:
-        already_installed = task.step("Checking if tools are installed", lambda: is_installed())
+        already_installed = task.command("Checking if tools are installed", ["xcode-select", "-p"]).returncode == 0
         if already_installed:
-            task.step("Tools are already installed")
+            task.msg("Tools are already installed")
             return
-        task.step("Triggering softwareupdate to show an update", lambda: trigger_install())
-        version = task.must_step("Determining lastest version", lambda: get_latest_version())
-        task.must_step(f"Installing version {version}", lambda: install_version(version))
+        task.command("Triggering softwareupdate to show an update", ["touch", "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"])
+        update_list = task.command("Getting update list", ["softwareupdate", "--list"])
+        version = None
+        for line in update_list.stdout.decode("utf-8").split("\n"):
+            if "Label: Command Line Tools" in line:
+                version = line.split(":")[1].strip()
+                break
+        task.command(f"Installing {version}", ["softwareupdate", "--install", version])
 
 
 install_xcode()
