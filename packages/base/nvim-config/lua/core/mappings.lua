60cf8e6a7eb8029edc117b836f726bc265d2c440
Tim Heuett <tim.heuett@gmail.com>
2025-01-02 09:53:25 -0800

    nvim: add split mappings

--- packages/base/nvim-config/lua/core/mappings.lua
+++ packages/base/nvim-config/lua/core/mappings.lua
@@ -3,6 +3,10 @@ vim.g.mapleader = ","
 -- quick exit to normal mode
 vim.keymap.set("i", "jj", "<ESC>")
 
+-- split shortcuts
+vim.keymap.set("n", "<leader>-", "<cmd>:split<cr>")
+vim.keymap.set("n", "<leader>\\", "<cmd>:vsplit<cr>")
+
 -- window movement
 vim.keymap.set("n", "<c-h>", "<c-w>h")
 vim.keymap.set("n", "<c-j>", "<c-w>j")
