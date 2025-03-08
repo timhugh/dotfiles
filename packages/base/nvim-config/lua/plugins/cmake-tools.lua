a7cb01e03c895cd01312efebf538619995bea77d
Tim Heuett <tim.heuett@gmail.com>
2025-03-08 15:14:48 -0800

    nvim: don't specific build config for cmake-tools (that's what presets are for)

--- packages/base/nvim-config/lua/plugins/cmake-tools.lua
+++ packages/base/nvim-config/lua/plugins/cmake-tools.lua
@@ -1,12 +1,5 @@
 return {
   "Civitasv/cmake-tools.nvim",
-  opts = {
-    cmake_generate_options = {
-      "-DBUILD_TESTING=ON",
-      "-DCMAKE_EXPORT_COMPILE_COMMANDS=1",
-      "-DCMAKE_BUILD_TYPE=Debug",
-    },
-    cmake_build_directory = "build",
-  },
+  opts = { },
 }
 
