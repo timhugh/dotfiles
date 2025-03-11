5b228595f84d865b89669eeda3cbab0ef8182ad8
Tim Heuett <tim.heuett@gmail.com>
2025-03-10 19:04:28 -0700

    nvim: add cmake keybinds

---
+++ packages/base/nvim-config/after/ftplugin/cpp.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>cb", ":CMakeBuild<CR>", { desc = "Build CMake project" })
+vim.keymap.set("n", "<leader>cr", ":CMakeRun<CR>", { desc = "Run CMake project" })
+vim.keymap.set("n", "<leader>cC", ":CMakeClean<CR>", { desc = "Clean CMake project" })
