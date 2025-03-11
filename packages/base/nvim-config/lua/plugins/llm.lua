e3889f6b879fcb3072af596765a577d4687ddb66
Tim Heuett <tim.heuett@gmail.com>
2025-03-10 19:04:07 -0700

    nvim: change llm keybinds

--- packages/base/nvim-config/lua/plugins/llm.lua
+++ packages/base/nvim-config/lua/plugins/llm.lua
@@ -2,12 +2,13 @@ return {
   {
     "ggml-org/llama.vim",
     init = function()
-      -- TODO: it doesn't seem like these color hints are being used
       vim.api.nvim_set_hl(0, "llama_hl_hint", {fg = "#999999", ctermfg="lightgray"})
       vim.api.nvim_set_hl(0, "llama_hl_info", {fg = "#444444", ctermfg="darkgray"})
 
       vim.g.llama_config = {
         show_info = false,
+        keymap_accept_full = "<S-M-Tab>",
+        keymap_accept_line = "<S-Tab>"
       }
     end,
   },
