dbb0e159a5e622b8ce8682644027276b9c540269
Tim Heuett <tim.heuett@gmail.com>
2025-03-11 12:41:39 -0700

    nvim vscode: add debug mappings

--- packages/base/nvim-config/lua/vscode-keymaps.lua
+++ packages/base/nvim-config/lua/vscode-keymaps.lua
@@ -12,7 +12,7 @@ keymap("n", "<leader>R", "<cmd>lua require('vscode').action('vscode-neovim.resta
 keymap("n", "<leader>t", "<cmd>lua require('vscode').action('workbench.action.terminal.toggleTerminal')<CR>", opts)
 keymap("n", "<leader>ff", "<cmd>lua require('vscode').action('workbench.action.quickOpen')<CR>", opts)
 keymap("n", "<leader>fG", "<cmd>lua require('vscode').action('workbench.action.findInFiles')<CR>", opts)
-keymap("n", "<leader>fr", "<cmd>lua require('vscode').action('workbench.action.openRecent')<CR>", opts)
+keymap("n", "<leader>fp", "<cmd>lua require('vscode').action('workbench.action.openRecent')<CR>", opts)
 keymap("n", "<leader>fg", "<cmd>lua require('vscode').action('git.openAllChanges')<CR>", opts)
 keymap("n", "<leader>r", "<cmd>lua require('vscode').action('editor.action.rename')<CR>", opts)
 keymap("n", "<leader>q", "<cmd>lua require('vscode').action('editor.action.quickFix')<CR>", opts)
@@ -20,6 +20,16 @@ keymap("n", "<leader>y", "<cmd>lua require('vscode').action('copyRelativeFilePat
 keymap("n", "<leader>o", "<cmd>lua require('vscode').action('workbench.view.explorer')<CR>", opts)
 keymap("n", "<leader>O", "<cmd>lua require('vscode').action('workbench.action.toggleSidebarVisibility')<CR>", opts)
 keymap("n", "<leader>z", "<cmd>lua require('vscode').action('workbench.action.toggleZenMode')<CR>", opts)
+
+-- debugger
+keymap("n", "<leader>dl", "<cmd>lua require('vscode').action('extension.js-debug.debugLink')<CR>", opts)
+keymap("n", "<leader>dd", "<cmd>lua require('vscode').action('workbench.action.debug.start')<CR>", opts)
+keymap("n", "<leader>db", "<cmd>lua require('vscode').action('editor.debug.action.toggleBreakpoint')<CR>", opts)
+keymap("n", "<leader>dc", "<cmd>lua require('vscode').action('editor.debug.action.clearBreakpoints')<CR>", opts)
+keymap("n", "<leader>dr", "<cmd>lua require('vscode').action('editor.debug.action.restart')<CR>", opts)
+keymap("n", "<leader>ds", "<cmd>lua require('vscode').action('editor.debug.action.stepOver')<CR>", opts)
+keymap("n", "<leader>di", "<cmd>lua require('vscode').action('editor.debug.action.stepInto')<CR>", opts)
+keymap("n", "<leader>do", "<cmd>lua require('vscode').action('editor.debug.action.stepOut')<CR>", opts)
 
 -- error navigation
 keymap("n", "<leader>E", "<cmd>lua require('vscode').action('editor.action.marker.this')<CR>", opts)
