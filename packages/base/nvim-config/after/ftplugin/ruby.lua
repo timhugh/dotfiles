4a3a1a2e853b1c0430d487d6180ea5beb9ac72c8
Tim Heuett <tim.heuett@gmail.com>
2025-05-30 19:17:09 -0700

    move rails keymaps to ruby plugin

--- packages/base/nvim-config/after/ftplugin/ruby.lua
+++ packages/base/nvim-config/after/ftplugin/ruby.lua
@@ -6,4 +6,11 @@ vim.opt.autoindent = true
 vim.opt.wrap = false
 
 -- vim.api.nvim_create_user_command('RailsRoutes', 'tabnew | r !bin/rails routes')
-
+vim.keymap.set("n", "<leader>gv", "<cmd>:Eview<cr>", { desc = "(Rails) Navigate to view" })
+vim.keymap.set("n", "<leader>tgv", "<cmd>:Tview<cr>", { desc = "(Rails) New tab at view" })
+vim.keymap.set("n", "<leader>xgv", "<cmd>:Sview<cr>", { desc = "(Rails) New hsplit at view" })
+vim.keymap.set("n", "<leader>vgv", "<cmd>:Vview<cr>", { desc = "(Rails) New vsplit at view" })
+vim.keymap.set("n", "<leader>gc", "<cmd>:Econtroller<cr>", { desc = "(Rails) Navigate to controller" })
+vim.keymap.set("n", "<leader>tgc", "<cmd>:Tcontroller<cr>", { desc = "(Rails) New tab at controller" })
+vim.keymap.set("n", "<leader>xgc", "<cmd>:Scontroller<cr>", { desc = "(Rails) New hsplit at controller" })
+vim.keymap.set("n", "<leader>vgc", "<cmd>:Vcontroller<cr>", { desc = "(Rails) New vsplit at controller" })
--- packages/base/nvim-config/lua/plugins/ruby.lua
+++ packages/base/nvim-config/lua/plugins/ruby.lua
@@ -1,17 +1,6 @@
 return {
   {
     "tpope/vim-rails",
-    keys = {
-      { "<leader>gv", "<cmd>:Eview<cr>", desc = "(Rails) Navigate to view" },
-      { "<leader>tgv", "<cmd>:Tview<cr>", desc = "(Rails) New tab at view" },
-      { "<leader>xgv", "<cmd>:Sview<cr>", desc = "(Rails) New hsplit at view" },
-      { "<leader>vgv", "<cmd>:Vview<cr>", desc = "(Rails) New vsplit at view" },
-
-      { "<leader>gc", "<cmd>:Econtroller<cr>", desc = "(Rails) Navigate to controller" },
-      { "<leader>tgc", "<cmd>:Tcontroller<cr>", desc = "(Rails) New tab at controller" },
-      { "<leader>xgc", "<cmd>:Scontroller<cr>", desc = "(Rails) New hsplit at controller" },
-      { "<leader>vgc", "<cmd>:Vcontroller<cr>", desc = "(Rails) New vsplit at controller" },
-    },
     ft = "ruby",
   },
 }
