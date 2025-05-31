1124a0c536f7bea646d23b1074d1675c2a802f7b
Tim Heuett <tim.heuett@gmail.com>
2025-05-30 19:09:17 -0700

    reconnect cmp to lsp

--- packages/base/nvim-config/lua/plugins/completions.lua
+++ packages/base/nvim-config/lua/plugins/completions.lua
@@ -1,12 +1,21 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    dependencies = {},
+    dependencies = {
+      'tzachar/cmp-ai',
+      'hrsh7th/cmp-nvim-lsp',
+    },
     opts = function()
       local cmp = require('cmp')
       return {
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
         sources = {
+          { name = 'nvim_lsp' },
           { name = 'codeium' },
+          { name = 'cmp_ai' },
         },
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -19,14 +28,37 @@ return {
     end
   },
   {
-    "Exafunction/windsurf.nvim",
+    'tzachar/cmp-ai',
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
+      'nvim-lua/plenary.nvim',
     },
-    config = function()
-      require("codeium").setup({
+    opts = function()
+      local cmp_ai = require('cmp_ai.config')
+      cmp_ai:setup({
+        max_lines = 1000,
+        provider = 'OpenAI',
+        provider_options = {
+          model = 'gpt-4',
+        },
+        notify = true,
+        notify_callback = function(msg)
+          vim.notify(msg)
+        end,
+        run_on_every_keystroke = true,
+        ignored_file_types = {
+        },
       })
-    end
+    end,
   },
+{
+  "Exafunction/windsurf.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    require("codeium").setup({
+    })
+  end
+},
 }
