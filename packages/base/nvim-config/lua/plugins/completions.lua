7cefb8570ee9c74658e5fd58691f59f9bbce615e
Tim Heuett <tim.heuett@gmail.com>
2025-06-03 18:04:59 -0700

    swap minuet back to codeium, avante deprecation warnings

--- packages/base/nvim-config/lua/plugins/completions.lua
+++ packages/base/nvim-config/lua/plugins/completions.lua
@@ -12,6 +12,7 @@ return {
         },
         sources = {
           { name = 'nvim_lsp' },
+          { name = 'codeium' },
           -- { name = 'minuet' },
         },
         performance = {
@@ -28,101 +29,112 @@ return {
     end
   },
   {
-    'milanglacier/minuet-ai.nvim',
-    opts = {
-      cmp = {
-        enable_auto_complete = false, -- toggle to enable cmp
-      },
-      virtualtext = {
-        auto_trigger_ft = { },
-        auto_trigger_ignore_ft = {},
-        keymap = {
-          accept = '<c-CR>',
-          accept_line = nil,
-          accept_n_lines = nil,
-          -- Cycle to next completion item, or manually invoke completion
-          next = '<c-y>',
-          -- Cycle to prev completion item, or manually invoke completion
-          prev = '<c-Y>',
-          dismiss = '<c-esc>',
-        },
-        -- Whether show virtual text suggestion when the completion menu
-        -- (nvim-cmp or blink-cmp) is visible.
-        show_on_completion_menu = false,
-      },
-      presets = {
-        -- this is the default preset
-        ollama = {
-          provider = "openai_fim_compatible",
-          context_window = 2000,
-          throttle = 400,
-          debounce = 100,
-          stream = true,
-          provider_options = {
-            openai_fim_compatible = {
-              api_key = 'TERM',
-              name = 'Ollama',
-              end_point = 'http://localhost:11434/v1/completions',
-              model = 'qwen2.5-coder:7b',
-              optional = {
-                max_tokens = 256,
-                top_p = 0.9,
-              },
-            },
-          },
-        },
-        openrouter = {
-          provider = "openai_compatible",
-          context_window = 20000,
-          request_timeout = 4,
-          throttle = 3000,
-          debounce = 1000,
-          stream = true,
-          provider_options = {
-            openai_compatible = {
-              model = 'google/gemini-2.5-flash-preview',
-            },
-          },
-          model = 'google/gemini-2.5-flash-preview',
-        },
-      },
-      provider = "openai_fim_compatible",
-      context_window = 2000,
-      throttle = 400,
-      debounce = 100,
-      provider_options = {
-        openai = {
-          optional = {
-            max_tokens = 256,
-          },
-        },
-        gemini = {
-          optional = {
-            generationConfig = {
-              maxOutputTokens = 256,
-              thinkingConfig = {
-                thinkingBudget = 0,
-              },
-            },
-            safetySettings = {
-              category = "HARM_CATEGORY_DANGEROUS_CONTENT",
-              threshold = "BLOCK_ONLY_HIGH",
-            },
-          },
-        },
-        openai_fim_compatible = {
-          api_key = 'TERM',
-          name = 'Ollama',
-          end_point = 'http://localhost:11434/v1/completions',
-          model = 'qwen2.5-coder:7b',
-          optional = {
-            max_tokens = 256,
-            top_p = 0.9,
-          },
-        },
-      },
+    "Exafunction/windsurf.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
     },
+    config = function()
+      require("codeium").setup({
+      })
+    end
   },
+  -- {
+  --   'milanglacier/minuet-ai.nvim',
+  --   opts = {
+  --     cmp = {
+  --       enable_auto_complete = false, -- toggle to enable cmp
+  --     },
+  --     virtualtext = {
+  --       auto_trigger_ft = { },
+  --       auto_trigger_ignore_ft = {},
+  --       keymap = {
+  --         accept = '<c-CR>',
+  --         accept_line = nil,
+  --         accept_n_lines = nil,
+  --         -- Cycle to next completion item, or manually invoke completion
+  --         next = '<c-y>',
+  --         -- Cycle to prev completion item, or manually invoke completion
+  --         prev = '<c-Y>',
+  --         dismiss = '<c-esc>',
+  --       },
+  --       -- Whether show virtual text suggestion when the completion menu
+  --       -- (nvim-cmp or blink-cmp) is visible.
+  --       show_on_completion_menu = false,
+  --     },
+  --     presets = {
+  --       -- this is the default preset
+  --       ollama = {
+  --         provider = "openai_fim_compatible",
+  --         context_window = 2000,
+  --         throttle = 400,
+  --         debounce = 100,
+  --         stream = true,
+  --         provider_options = {
+  --           openai_fim_compatible = {
+  --             api_key = 'TERM',
+  --             name = 'Ollama',
+  --             end_point = 'http://localhost:11434/v1/completions',
+  --             model = 'qwen2.5-coder:7b',
+  --             optional = {
+  --               max_tokens = 256,
+  --               top_p = 0.9,
+  --             },
+  --           },
+  --         },
+  --       },
+  --       openrouter = {
+  --         provider = "openai_compatible",
+  --         context_window = 20000,
+  --         request_timeout = 4,
+  --         throttle = 3000,
+  --         debounce = 1000,
+  --         stream = true,
+  --         provider_options = {
+  --           openai_compatible = {
+  --             model = 'google/gemini-2.5-flash-preview',
+  --           },
+  --         },
+  --         model = 'google/gemini-2.5-flash-preview',
+  --       },
+  --     },
+  --     provider = "openai_fim_compatible",
+  --     context_window = 2000,
+  --     throttle = 400,
+  --     debounce = 100,
+  --     provider_options = {
+  --       openai = {
+  --         optional = {
+  --           max_tokens = 256,
+  --         },
+  --       },
+  --       gemini = {
+  --         optional = {
+  --           generationConfig = {
+  --             maxOutputTokens = 256,
+  --             thinkingConfig = {
+  --               thinkingBudget = 0,
+  --             },
+  --           },
+  --           safetySettings = {
+  --             category = "HARM_CATEGORY_DANGEROUS_CONTENT",
+  --             threshold = "BLOCK_ONLY_HIGH",
+  --           },
+  --         },
+  --       },
+  --       openai_fim_compatible = {
+  --         api_key = 'TERM',
+  --         name = 'Ollama',
+  --         end_point = 'http://localhost:11434/v1/completions',
+  --         model = 'qwen2.5-coder:7b',
+  --         optional = {
+  --           max_tokens = 256,
+  --           top_p = 0.9,
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
@@ -134,30 +146,34 @@ return {
         auto_suggestions = false,
         enable_cursor_planning_mode = true,
       },
-      ollama = {
-        endpoint = "http://127.0.0.1:11434",
-        model = "qwen2.5-coder:14b",
-        disable_tools = true,
-      },
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o",
-        timeout = 30000,
-        temperature = 0,
-        max_completion_tokens = 8192,
-      },
-      claude = {
-        endpoint = "https://api.anthropic.com",
-        model = "claude-3-5-sonnet-20241022",
-        temperature = 0,
-        max_tokens = 4096,
-      },
-      vendors = {
+      providers = {
+        ollama = {
+          endpoint = "http://127.0.0.1:11434",
+          model = "qwen2.5-coder:14b",
+          disable_tools = true,
+        },
         openrouter = {
           __inherited_from = 'openai',
           endpoint = 'https://openrouter.ai/api/v1',
           api_key_name = 'OPENROUTER_API_KEY',
           model = 'google/gemini-2.5-flash-preview',
+        },
+        openai = {
+          endpoint = "https://api.openai.com/v1",
+          model = "gpt-4o",
+          timeout = 30000,
+          extra_request_body = {
+            max_completion_tokens = 8192,
+            temperature = 0,
+          },
+        },
+        claude = {
+          endpoint = "https://api.anthropic.com",
+          model = "claude-3-5-sonnet-20241022",
+          extra_request_body = {
+            max_tokens = 4096,
+            temperature = 0,
+          },
         },
       },
     },
