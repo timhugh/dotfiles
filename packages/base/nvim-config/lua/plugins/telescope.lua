268ed4095dffc8f147e4538f14669953159838b0
Tim Heuett <tim.heuett@gmail.com>
2025-06-11 12:40:42 -0700

    nvim: use fd; tweak telescope-dir settings; improve lazy load settings

--- packages/base/nvim-config/lua/plugins/telescope.lua
+++ packages/base/nvim-config/lua/plugins/telescope.lua
@@ -1,4 +1,3 @@
--- TODO: https://github.com/nvim-telescope/telescope-frecency.nvim
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -6,40 +5,44 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
-    init = function()
-      local builtin = require("telescope.builtin")
+    keys = {
+      -- file related bindings
+      { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Find buffers" },
+      { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find file" },
+      { "<leader>fgS", "<cmd>Telescope git_status<CR>", desc = "Find in git modified files vs HEAD" },
 
-      -- file-related bindings
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find file" })
-      vim.keymap.set("n", "<leader>fgS", builtin.git_status, { desc = "Find in git modified files vs HEAD" })
+      -- grep related bindings
+      { "<leader>fg", "<cmd>Telescope current_buffer_fuzzy_find<CR>", desc = "Grep current buffer" },
+      { "<leader>fG", "<cmd>Telescope live_grep<CR>", desc = "Grep all files" },
+      { "<leader>f*", "<cmd>Telescope grep_string<CR>", desc = "Grep all files (with current word)" },
 
-      -- grep-related bindings
-      vim.keymap.set("n", "<leader>fg", builtin.current_buffer_fuzzy_find, { desc = "Grep current buffer" })
-      vim.keymap.set("n", "<leader>fG", builtin.live_grep, { desc = "Grep all files" })
-      vim.keymap.set("n", "<leader>f*", builtin.grep_string, { desc = "Grep all files (with current word)" })
+      -- lsp related bindings
+      { "<leader>fr", "<cmd>Telescope lsp_references<CR>", desc = "Find references (LSP)" },
+      { "<leader>fs", "<cmd>Telescope lsp_document_symbols<CR>", desc = "Find symbols in current buffer (LSP)" },
+      { "<leader>fS", "<cmd>Telescope lsp_workspace_symbols<CR>", desc = "Find symbols in workspace (LSP)" },
 
-      -- lsp-related bindings
-      vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Find references (LSP)" })
-      vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Find symbols in current buffer (LSP)" })
-      vim.keymap.set("n", "<leader>fS", builtin.lsp_workspace_symbols, { desc = "Find symbols in workspace (LSP)" })
-
-      vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find mapped keys" })
-    end,
+      -- other
+      { "<leader>fk", "<cmd>Telescope keymaps<CR>", desc = "Find mapped keys" },
+    },
   },
   {
     "princejoogie/dir-telescope.nvim",
     dependencies = {
       "nvim-telescope/telescope.nvim",
     },
-    config = function()
-      require("dir-telescope").setup()
-    end,
+    keys = {
+      { "<leader>fdg", "<cmd>Telescope dir live_grep<CR>", desc = "Grep all files in directory" },
+      { "<leader>fdf", "<cmd>Telescope dir find_files<CR>", desc = "Find files in directory" },
+    },
     init = function()
       require("telescope").load_extension("dir")
-      vim.keymap.set("n", "<leader>fdg", "<cmd>Telescope dir live_grep<CR>", { desc = "Grep all files in directory" })
-      vim.keymap.set("n", "<leader>fdf", "<cmd>Telescope dir find_files<CR>", { desc = "Find files in directory" })
     end,
+    opts = {
+      hidden = false,
+      no_ignore = false,
+      show_preview = true,
+      follow_symlinks = false,
+    }
   },
   {
     "nvim-telescope/telescope-frecency.nvim",
@@ -47,10 +50,11 @@ return {
     dependencies = {
       'nvim-telescope/telescope.nvim',
     },
-    config = function()
+    keys = {
+      { "<leader>fR", "<cmd>Telescope frecency<CR>", desc = "Recent files" },
+    },
+    init = function()
       require("telescope").load_extension("frecency")
-      local frecency = require("telescope").extensions.frecency
-      vim.keymap.set("n", "<leader>fR", frecency.frecency, { desc = "Recent files" })
     end,
   },
   {
@@ -58,9 +62,11 @@ return {
     dependencies = {
       'nvim-telescope/telescope.nvim',
     },
-    config = function()
+    keys = {
+      { "<leader>fgs", "<cmd>Telescope git_branch<CR>", desc = "Find in git modified files vs default branch" },
+    },
+    init = function()
       require("telescope").load_extension("git_branch")
-      vim.keymap.set("n", "<leader>fgs", "<cmd>Telescope git_branch<CR>", { desc = "Find in git modified files vs default branch" })
     end,
   },
 }
