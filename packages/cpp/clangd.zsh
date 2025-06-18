8e17c67d3d54e64985fecafa43c057c2aeef3131
Tim Heuett <tim.heuett@gmail.com>
2025-06-18 13:20:51 -0700

    mise can handle global versions instead of root idiomatic files; configure mise llvm for sys sdk

--- packages/cpp/clangd.zsh
+++
@@ -1,5 +0,0 @@
-alias clang-tidy="xcrun clang-tidy"
-
-export CC=$(mise which clang)
-export CXX=$(mise which clang++)
-
---
+++ packages/cpp/llvm.zsh
@@ -0,0 +1,9 @@
+sys_root="$(xcrun --show-sdk-path)"
+
+export CC="$(mise which clang) -isysroot ${sys_root}"
+export CXX="$(mise which clang++) -isysroot ${sys_root}"
+
+export CFLAGS="-isysroot ${sys_root} ${CFLAGS}"
+export CXXFLAGS="-isysroot ${sys_root} ${CXXFLAGS}"
+export LDFLAGS="-isysroot ${sys_root} ${LDFLAGS}"
+
