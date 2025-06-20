d1f5f4730a6b5beaf66311d5a9fe66a16892b413
Tim Heuett <tim.heuett@gmail.com>
2025-06-20 13:28:08 -0700

    nvim: add troublel; get diagnostics for entire workspace with supported lsps

--- packages/base/nvim.config/lua/core/lsp.lua
+++ packages/base/nvim.config/lua/core/lsp.lua
@@ -1,3 +1,70 @@
+-- some LSPs (like copilot) are not actually LSPs and need to be handled differently at times
+local unsupported_lsps = {
+  copilot = true,
+}
+
+local function is_unsupported_lsp(client)
+  return unsupported_lsps[client.name] or false
+end
+
+local function is_supported_lsp(client)
+  return not is_unsupported_lsp(client)
+end
+
+-- This is adapted from https://artem.rocks/posts/workspace_diagnostics_nvim
+-- I added some extra checks to ensure that the client is actually going to provide diagnostics correctly (e.g. not copilot)
+--
+-- It works by using git ls-files at launch to get all files in the workspace, then triggering diagnostics for each file.
+-- There is probably going to be some testing needed with different LSPs, and I have a little bit of concern around caching
+-- all of the files at launch and whether or not that will be a performance problem, and also whether or not it will update
+-- correctly as files are changed.
+local function lsp_supports_workspace_diagnostics(client)
+  return vim.tbl_get(client.server_capabilities, 'textDocumentSync', 'openClose') and is_supported_lsp(client)
+end
+
+local loaded_clients = {}
+local function trigger_workspace_diagnostics(client, bufnr, workspace_files)
+  if vim.tbl_contains(loaded_clients, client.id) then
+    print("Client", client.name, "already processed, skipping")
+    return
+  end
+  table.insert(loaded_clients, client.id)
+
+  if not lsp_supports_workspace_diagnostics(client) then
+    print("Client", client.name, "does not support workspace diagnostics, skipping")
+    return
+  end
+
+  for _, path in ipairs(workspace_files) do
+    if path == vim.api.nvim_buf_get_name(bufnr) then
+      goto continue
+    end
+
+    local filetype = vim.filetype.match({ filename = path })
+
+    if not vim.tbl_contains(client.config.filetypes, filetype) then
+      goto continue
+    end
+
+    local params = {
+      textDocument = {
+        uri = vim.uri_from_fname(path),
+        version = 0,
+        text = vim.fn.join(vim.fn.readfile(path), "\n"),
+        languageId = filetype
+      }
+    }
+    client.notify('textDocument/didOpen', params)
+
+    ::continue::
+  end
+end
+
+local workspace_files = vim.fn.split(vim.fn.system('git ls-files'), "\n")
+workspace_files = vim.tbl_map(function(path)
+  return vim.fn.fnamemodify(path, ":p")
+end, workspace_files)
+
 local function format_changed_hunks()
   local hunks = require('gitsigns').get_hunks()
 
@@ -27,7 +94,7 @@ local on_attach = function(client, bufnr)
       callback = format_changed_hunks,
     })
   end
-
+  trigger_workspace_diagnostics(client, bufnr, workspace_files)
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
   vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
   vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { desc = "Go to implementation" })
@@ -83,9 +150,7 @@ vim.api.nvim_create_user_command('LspRestart', function()
   local bufnr = vim.api.nvim_get_current_buf()
   local clients = vim.lsp.get_clients({ bufnr = bufnr })
   for _, client in ipairs(clients) do
-    if client.name == "copilot" then
-      -- don't try to restart copilot, it's not a real LSP server
-    else
+    if is_supported_lsp(client) then
       vim.lsp.enable(client.name, false)
       vim.lsp.enable(client.name, true)
     end
