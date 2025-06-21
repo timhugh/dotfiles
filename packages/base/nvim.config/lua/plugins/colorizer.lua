5eb16b8db759e0c15ad6d97de0766f1db8d8680e
Tim Heuett <tim.heuett@gmail.com>
2025-06-20 17:00:08 -0700

    nvim: WIP get colorizer working with tailwind custom colors

--- packages/base/nvim.config/lua/plugins/colorizer.lua
+++ packages/base/nvim.config/lua/plugins/colorizer.lua
@@ -1,13 +1,31 @@
 return {
   {
     "catgoose/nvim-colorizer.lua",
-    event = "BufReadPre",
+    event = "VeryLazy",
     opts = {
+      lazy_load = true,
       filetypes = { "*" },
       user_default_options = {
-        names = false,
-        tailwind = true,
+        names = true,
+        names_opts = {
+          lowercase = true,
+          camelcase = true,
+          uppercase = true,
+          strip_digits = false,
+        },
+        RGB = true,       -- #RGB hex codes
+        RGBA = true,      -- #RGBA hex codes
+        RRGGBB = true,    -- #RRGGBB hex codes
+        RRGGBBAA = false, -- #RRGGBBAA hex codes
+        AARRGGBB = false, -- 0xAARRGGBB hex codes
+        rgb_fn = false,   -- CSS rgb() and rgba() functions
+        hsl_fn = false,   -- CSS hsl() and hsla() functions
+        tailwind = "lsp", -- options are boolean|"normal"|"lsp"|"both" (true is "normal")
+        -- "normal" will use the default tailwind config
+        -- "lsp" will get custom colors from the LSP
+        -- "both" will combine the two
         tailwind_opts = {
+          -- this must be set to true to use LSP results
           update_names = true,
         },
       },
