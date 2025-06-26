a4fc0ff22fdebdc167a510c10920aa04424b2ea6
Tim Heuett <tim.heuett@gmail.com>
2025-06-26 16:38:36 -0700

    vim: use bujo.nvim new keybind configs

--- packages/base/nvim.config/lua/plugins/bujo.lua
+++ packages/base/nvim.config/lua/plugins/bujo.lua
@@ -3,32 +3,21 @@ return {
     dir = "~/git/bujo.nvim",
     enabled = true,
     lazy = true,
+    event = "VeryLazy",
     dependencies = {
       "nvim-telescope/telescope.nvim",
       "leafo/etlua",
     },
-    keys = {
-      { "<leader>nn", "<CMD>Bujo now<CR>", { desc = "Bujo: Open current week" } },
-      { "<leader>nN", "<CMD>Bujo note<CR>", { desc = "Bujo: Create new note" } },
-      { "<leader>fn", "<CMD>Telescope bujo<CR>", { desc = "Bujo: Open note picker" } },
-    },
     opts = {
       entries_template = "weekly-entry.etlua",
+      telescope_picker_keybind = "<leader>fn",
+      auto_commit_journal = true,
+      auto_push_journal = true,
     },
     config = function(_, opts)
       require("bujo").setup(opts)
 
       require("telescope").load_extension("bujo")
-
-      vim.api.nvim_create_autocmd("BufEnter", {
-        pattern = { "*.md" },
-        callback = function()
-          vim.api.nvim_set_keymap("n", "gf", "<CMD>Bujo follow<CR>",
-            { desc = "Bujo: Follow link", noremap = true, silent = true })
-          vim.api.nvim_set_keymap("n", "<C-Space>", "<CMD>Bujo togglecheck<CR>",
-            { desc = "Bujo: Toggle checkbox", noremap = true, silent = true })
-        end,
-      })
     end,
   },
 }
