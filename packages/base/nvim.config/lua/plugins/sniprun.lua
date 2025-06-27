f816b40ed4420114aaf1794041ec5bd9dadca04c
Tim Heuett <tim.heuett@gmail.com>
2025-06-27 12:14:21 -0700

    add deno and enable repl functionality in sniprun

--- packages/base/nvim.config/lua/plugins/sniprun.lua
+++ packages/base/nvim.config/lua/plugins/sniprun.lua
@@ -5,11 +5,18 @@ return {
     lazy = true,
     event = "VeryLazy",
     branch = "master",
-    build = "bash ./install.sh",
+    build = "cargo build --release",
     cmd = { "SnipRun" },
     keys = {
       { "<leader>r", "<cmd>SnipRun<cr>", desc = "SnipRun: Run code block", mode = { "n", "v" } },
     },
-    opts = {},
+    opts = {
+      selected_interpreters = {
+        "JS_TS_deno",
+      },
+      repl_enable = {
+        "JS_TS_deno",
+      },
+    },
   },
 }
