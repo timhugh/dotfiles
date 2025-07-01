78cccb80aa2914e8d86ed6dfbd08b2a61274e4c5
Tim Heuett <tim.heuett@gmail.com>
2025-06-30 20:46:18 -0700

    add dashboard-nvim

---
+++ packages/base/nvim.config/lua/plugins/dashboard.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    'nvimdev/dashboard-nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons'
+    },
+    event = 'VimEnter',
+    keys = {
+      { '<leader>H', '<cmd>Dashboard<cr>', desc = 'Open Dashboard' },
+    },
+    opts = {
+      theme = 'hyper',
+      config = {
+        packages = {
+          enable = false,
+        },
+        week_header = {
+          enable = true,
+        },
+        shortcut = {
+          {
+            icon = '',
+            desc = 'Edit dotfiles',
+            group = 'DashboardShortCut',
+            action = 'cd ~/.dotfiles | edit .',
+            key = 'D',
+          },
+          {
+            icon = '',
+            desc = 'Journal',
+            group = 'DashboardShortCut',
+            action = 'Bujo now',
+            key = 'J',
+          },
+        },
+      },
+    },
+  },
+}
