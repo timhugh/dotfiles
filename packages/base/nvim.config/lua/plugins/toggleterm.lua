060ab3dee93e69a39b4d8b21402038650207769d
Tim Heuett <tim.heuett@gmail.com>
2025-08-26 23:49:22 -0700

    nvim: add toggleterm back

---
+++ packages/base/nvim.config/lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    keys = {
+      { "<leader>tt", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+    },
+    opts = {
+      shade_terminals = false,
+    },
+  },
+}
