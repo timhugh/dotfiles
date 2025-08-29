f988993ab8e097501ca32b9f29cacc7418055ec8
Tim Heuett <tim.heuett@gmail.com>
2025-08-28 17:18:49 -0700

    nvim: tweak next diagnostic/git hunk mappings

--- packages/base/nvim.config/lua/plugins/git.lua
+++ packages/base/nvim.config/lua/plugins/git.lua
@@ -10,8 +10,8 @@ return {
       require("gitsigns").setup()
 
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<cr>", { desc = "Preview current hunk" })
-      vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk<cr>", { desc = "Jump to next hunk" })
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns prev_hunk<cr>", { desc = "Jump to previous hunk" })
+      vim.keymap.set("n", "]g", ":Gitsigns next_hunk<cr>", { desc = "Jump to next hunk" })
+      vim.keymap.set("n", "[g", ":Gitsigns prev_hunk<cr>", { desc = "Jump to previous hunk" })
       vim.keymap.set("n", "<leader>gR", ":Gitsigns reset_hunk<cr>", { desc = "Reset current hunk" })
       vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk<cr>", { desc = "Stage current hunk" })
       vim.keymap.set("n", "<leader>gu", ":Gitsigns undo_stage_hunk<cr>", { desc = "Undo stage current hunk" })
