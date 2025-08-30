ccc0ed2f4215831c2509c3aa580cb7a28ee62a5c
Tim Heuett <tim.heuett@gmail.com>
2025-08-30 10:39:49 -0700

    nas: tweak borg compression

--- _nas/borg/b2-borg-create-backup.sh
+++ _nas/borg/b2-borg-create-backup.sh
@@ -6,7 +6,7 @@ source "${HOME}/.secrets"
 archive_name="vault-$(date +%Y%m%dT%H%M%S)"
 echo "$(date) - BORG: creating backup ${archive_name}"
 
-borg_options="--stats --compression zstd,22"
+borg_options="--stats --compression zstd,6 --progress"
 
 borg create ${borg_options} /mnt/vault/backups::${archive_name} /mnt/vault/shared
 echo "$(date) - BOR: backup created"
