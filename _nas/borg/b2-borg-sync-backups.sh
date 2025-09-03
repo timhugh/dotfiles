2c51efa73e043e7489f4abc800d5ef7052d9aa9f
Tim Heuett <tim.heuett@gmail.com>
2025-09-02 18:00:23 -0700

    nas: config rclone and enable timer

--- _nas/borg/b2-borg-sync-backups.sh
+++ _nas/borg/b2-borg-sync-backups.sh
@@ -3,13 +3,12 @@
 # set B2_BORG_VAULT_NAME
 source "${HOME}/.secrets"
 
-export TMPDIR=/mnt/vault/tmp
-
 echo "$(date) - B2: syncing updates"
-rclone sync /mnt/vault/backups b2:$B2_BORG_VAULT_NAME --transfers=1 --verbose
+export RCLONE_FAST_LIST=1
+rclone copy /mnt/vault/backups b2:$B2_BORG_VAULT_NAME --transfers=10 --verbose --progress
 echo "$(date) - B2: sync complete"
 
 # this step deletes any partial files from failed syncs
 echo "$(date) - B2: cleaning up temp files"
-rclone cleanup b2:$vault --verbose
+rclone cleanup b2:$B2_BORG_VAULT_NAME --verbose
 
