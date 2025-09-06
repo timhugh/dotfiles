a7efc5f9d2a01c987be62393a1f99ae0e71e83ce
Tim Heuett <tim.heuett@gmail.com>
2025-09-06 09:30:30 -0700

    spacing

--- packages/base/nvim.config/lua/core/mappings.lua
+++ packages/base/nvim.config/lua/core/mappings.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = ","
 -- split shortcuts
 vim.keymap.set("n", "<c-->", "<cmd>:split<cr>")
 vim.keymap.set("n", "<leader>-", "<cmd>:split<cr>")
+
 vim.keymap.set("n", "<c-\\>", "<cmd>:vsplit<cr>")
 vim.keymap.set("n", "<leader>\\", "<cmd>:vsplit<cr>")
 
@@ -19,12 +20,15 @@ vim.keymap.set("n", "<leader>t-", "<cmd>:split | wincmd j | terminal<cr>")
 vim.keymap.set("n", "<m-h>", "<c-w>h")
 vim.keymap.set("n", "<c-h>", "<c-w>h")
 vim.keymap.set("t", "<m-h>", "<c-\\><c-n><c-w>h")
+
 vim.keymap.set("n", "<m-j>", "<c-w>j")
 vim.keymap.set("n", "<c-j>", "<c-w>j")
 vim.keymap.set("t", "<m-j>", "<c-\\><c-n><c-w>j")
+
 vim.keymap.set("n", "<m-k>", "<c-w>k")
 vim.keymap.set("n", "<c-k>", "<c-w>k")
 vim.keymap.set("t", "<m-k>", "<c-\\><c-n><c-w>k")
+
 vim.keymap.set("n", "<m-l>", "<c-w>l")
 vim.keymap.set("n", "<c-l>", "<c-w>l")
 vim.keymap.set("t", "<m-l>", "<c-\\><c-n><c-w>l")
