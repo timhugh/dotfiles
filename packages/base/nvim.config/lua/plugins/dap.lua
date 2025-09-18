2d0c1834a373a1216c7e1ee52859a01fd5071e0c
Tim Heuett <tim.heuett@gmail.com>
2025-09-17 23:34:54 -0700

    nvim: testing dap ui

--- packages/base/nvim.config/lua/plugins/dap.lua
+++ packages/base/nvim.config/lua/plugins/dap.lua
@@ -30,4 +30,31 @@ return {
       vim.keymap.set("n", "<leader>dr", "<cmd>:DapToggleRepl<cr>")
     end,
   },
+  {
+    "rcarriga/nvim-dap-ui",
+    enabled = true,
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup()
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  }
 }
