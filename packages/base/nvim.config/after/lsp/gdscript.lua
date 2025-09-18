02c9739f688ad40ea7a1ff5b3d8dae5e01977e3b
Tim Heuett <tim.heuett@gmail.com>
2025-09-17 23:03:55 -0700

    nvim: start server in godot projects

--- packages/base/nvim.config/after/lsp/gdscript.lua
+++ packages/base/nvim.config/after/lsp/gdscript.lua
@@ -1,6 +1,12 @@
 local port = os.getenv 'GDScript_Port' or '6005'
 local cmd = vim.lsp.rpc.connect('127.0.0.1', tonumber(port))
 
+local server_pipe_path = vim.fn.getcwd() .. '/server.pipe'
+local is_server_running = vim.uv.fs_stat(server_pipe_path)
+if not is_server_running then
+    vim.fn.serverstart(server_pipe_path)
+end
+
 return {
   cmd = cmd,
   filetypes = { 'gd', 'gdscript', 'gdscript3' },
