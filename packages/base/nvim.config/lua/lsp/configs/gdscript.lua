f0c30f92bf4977630e10164489817bfad70d4abf
Tim Heuett <tim.heuett@gmail.com>
2025-09-23 13:56:50 -0700

    nvim: only start nvim server when actually attaching to a gdscript file
    although it might be neat to use that on other projects, but I'll cross that bridge later

--- packages/base/nvim.config/lua/lsp/configs/gdscript.lua
+++ packages/base/nvim.config/lua/lsp/configs/gdscript.lua
@@ -2,13 +2,15 @@ local port = os.getenv 'GDScript_Port' or '6005'
 local cmd = vim.lsp.rpc.connect('127.0.0.1', tonumber(port))
 
 local server_pipe_path = vim.fn.getcwd() .. '/server.pipe'
-local is_server_running = vim.uv.fs_stat(server_pipe_path)
-if not is_server_running then
-  vim.fn.serverstart(server_pipe_path)
-end
 
 require('lsp').configure_lsp('gdscript', {
   cmd = cmd,
   filetypes = { 'gd', 'gdscript', 'gdscript3' },
   root_markers = { 'project.godot', '.git' },
+  on_attach = function()
+    local is_server_running = vim.uv.fs_stat(server_pipe_path)
+    if not is_server_running then
+      vim.fn.serverstart(server_pipe_path)
+    end
+  end,
 })
