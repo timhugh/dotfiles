30212904e34ec1f9f05ef8a567b3eee14781fa6d
Tim Heuett <tim.heuett@gmail.com>
2025-09-28 00:43:11 -0700

    nvim: override goto def for ruby-lsp
    doesn't seem to work with the tagfunc

--- packages/base/nvim.config/lua/lsp/configs/ruby_lsp.lua
+++ packages/base/nvim.config/lua/lsp/configs/ruby_lsp.lua
@@ -5,4 +5,9 @@ require('lsp').configure_lsp('ruby_lsp', {
   init_options = {
     formatter = 'auto',
   },
+  on_attach = function()
+    -- for some reason the tagfunc doesn't seem to work for ruby_lsp
+    -- so this is kind of a brute force way to get "go to definition" working
+    vim.keymap.set("n", "<C-]>", "<cmd>lua vim.lsp.buf.definition()<CR>", { buffer = true, desc = "Go to definition" })
+  end,
 })
