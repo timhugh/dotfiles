16fc08d79f6d29e1a90c0719f04a946042fd0c91
Tim Heuett <tim.heuett@gmail.com>
2025-10-10 23:55:57 -0700

    add nvim nightly

--- packages/base/nvim.zsh
+++ packages/base/nvim.zsh
@@ -1,4 +1,8 @@
-export EDITOR='nvim'
+if [[ -d ${HOME}/share/nvim-macos-arm64 ]]; then
+    export EDITOR="${HOME}/share/nvim-macos-arm64/bin/nvim"
+else
+    export EDITOR='nvim'
+fi
 alias e="$EDITOR"
 alias ev="neovide"
 
