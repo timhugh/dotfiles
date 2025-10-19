09de170f5441538875ecfda2599e065c2a66b2cb
Tim Heuett <tim.heuett@gmail.com>
2025-10-19 00:53:17 -0700

    nvim: temp disable autoformat in html

--- packages/base/nvim.config/lua/lsp/configs/html.lua
+++ packages/base/nvim.config/lua/lsp/configs/html.lua
@@ -8,4 +8,10 @@ require('lsp').configure_lsp('html', {
     embeddedLanguages = { css = true, javascript = true },
     configurationSection = { 'html', 'css', 'javascript' },
   },
+  on_attach = function(client)
+    -- html has an annoying habit of removing new lines in weird places
+    -- so I'm just disabling the formatting for it for now
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
+  end,
 })
