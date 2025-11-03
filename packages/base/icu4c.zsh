ad17c49abf17f16c4394d085cb3632d14ababd79
Tim Heuett <tim.heuett@gmail.com>
2025-11-03 00:31:17 -0800

    WIP: linux installer

--- packages/base/icu4c.zsh
+++ packages/base/icu4c.zsh
@@ -1,9 +1,10 @@
-icu4c="$(brew --prefix icu4c)"
+if command -v brew; then
+	icu4c="$(brew --prefix icu4c)"
 
-export PATH="$icu4c/bin:$PATH"
-export PATH="$icu4c/sbin:$PATH"
+	export PATH="$icu4c/bin:$PATH"
+	export PATH="$icu4c/sbin:$PATH"
 
-export LDFLAGS="-L$icu4c/lib $LDFLAGS"
-export CPPFLAGS="-I$icu4c/include $CPPFLAGS"
-export PKG_CONFIG_PATH="$icu4c/lib/pkgconfig:$PKG_CONFIG_PATH"
-
+	export LDFLAGS="-L$icu4c/lib $LDFLAGS"
+	export CPPFLAGS="-I$icu4c/include $CPPFLAGS"
+	export PKG_CONFIG_PATH="$icu4c/lib/pkgconfig:$PKG_CONFIG_PATH"
+fi
