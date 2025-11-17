5f6c454909a1b3ea0bc6c9269100eb6750d4ee50
Tim Heuett <tim.heuett@gmail.com>
2025-11-17 09:49:23 -0800

    nvim: add avante back

---
+++ packages/base/nvim.config/lua/plugins/avante.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "yetone/avante.nvim",
+    build = "make",
+    event = "VeryLazy",
+    version = false,
+    ---@module 'avante'
+    ---@type avante.Config
+    opts = {
+      provider = "copilot",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-telescope/telescope.nvim",
+      "zbirenbaum/copilot.lua",
+    },
+  },
+}
