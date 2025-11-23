76a4725ed9ea2d22d22e6b093966aacd2a53c979
Tim Heuett <tim.heuett@gmail.com>
2025-11-23 13:33:14 -0800

    nvim: reuse ruby-lsp clients

--- packages/base/nvim.config/lua/lsp/configs/ruby_lsp.lua
+++ packages/base/nvim.config/lua/lsp/configs/ruby_lsp.lua
@@ -5,6 +5,10 @@ require('lsp').configure_lsp('ruby_lsp', {
   init_options = {
     formatter = 'auto',
   },
+  reuse_client = function(client, config)
+    config.cmd_cwd = config.root_dir
+    return client.config.cmd_cwd == config.cmd_cwd
+  end,
   on_attach = function()
     -- for some reason the tagfunc doesn't seem to work for ruby_lsp
     -- so this is kind of a brute force way to get "go to definition" working
