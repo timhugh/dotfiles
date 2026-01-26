5830968790f6a89e6e531dbd76590c636d1f362b
Tim Heuett <tim.heuett@gmail.com>
2026-01-26 09:17:55 -0800

    testing copilot NES
    needs some work

--- packages/base/nvim.config/lua/plugins/copilot.lua
+++ packages/base/nvim.config/lua/plugins/copilot.lua
@@ -2,28 +2,41 @@ return {
   {
     "zbirenbaum/copilot.lua",
     enabled = true,
+    dependencies = {
+      "copilotlsp-nvim/copilot-lsp",
+    },
     cmd = "Copilot",
     event = "InsertEnter",
     opts = {
       copilot_node_command = {"mise", "x", "node@lts", "--", "node" },
-      keymap = {
-        jump_prev = "[[",
-        jump_next = "]]",
-        accept = "<c-y>",
-        refresh = "gr",
-        open = "<M-CR>",
-      },
       suggestion = {
         enabled = true,
         auto_trigger = true,
+        keymap = {
+          accept = "<M-Tab>",
+        },
       },
       panel = {
         enabled = false,
+      },
+      nes = {
+        enabled = false,
+        auto_trigger = true,
+        keymap = {
+          accept_and_goto = "<S-M-Tab>",
+        },
       },
       filetypes = {
         markdown = false,
         ["*"] = true,
       },
     },
+  },
+  {
+    "copilotlsp-nvim/copilot-lsp",
+    enabled = false,
+    init = function()
+      vim.g.copilot_nes_debounce = 500
+    end,
   },
 }
