1a6490a4fcdb29f43e0730ff447ceb162a579728
Tim Heuett <tim.heuett@gmail.com>
2026-03-06 10:51:04 -0800

    nvim: add dispatch binds

--- packages/base/nvim.config/lua/plugins/dispatch.lua
+++ packages/base/nvim.config/lua/plugins/dispatch.lua
@@ -2,6 +2,10 @@ return {
   {
     "tpope/vim-dispatch",
     enabled = true,
+    keys = {
+      { "<leader>M", "<cmd>:Make<cr>", desc = "Dispatch: run make" },
+      { "<leader>T", "<cmd>:Make test<cr>", desc = "Dispatch: run make test" },
+    },
     config = function()
     end,
   }
