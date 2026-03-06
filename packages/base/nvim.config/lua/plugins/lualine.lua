2277bb1da57ce89cb0349637d7fed6bada4bb78e
Tim Heuett <tim.heuett@gmail.com>
2026-03-06 11:38:24 -0800

    nvim: customize lualine config

--- packages/base/nvim.config/lua/plugins/lualine.lua
+++ packages/base/nvim.config/lua/plugins/lualine.lua
@@ -1,9 +1,75 @@
+local filename = {
+  'filename',
+  path = 1,
+}
+
+local lsp_status = {
+  'lsp_status',
+  ignore_lsp = {
+    'copilot',
+  },
+}
+
 return {
   "nvim-lualine/lualine.nvim",
   enabled = true,
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  opts = {},
+  config = function()
+    require('lualine').setup {
+      options = {
+        icons_enabled = false,
+        theme = 'auto',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = false,
+        always_show_tabline = false,
+        globalstatus = false,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+          refresh_time = 16, -- ~60fps
+          events = {
+            'WinEnter',
+            'BufEnter',
+            'BufWritePost',
+            'SessionLoadPost',
+            'FileChangedShellPost',
+            'VimResized',
+            'Filetype',
+            'CursorMoved',
+            'CursorMovedI',
+            'ModeChanged',
+          },
+        }
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = { filename, 'filetype' },
+        lualine_x = { 'diagnostics' },
+        lualine_y = { lsp_status },
+        lualine_z = { 'location' }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { filename },
+        lualine_x = { 'diagnostics', lsp_status },
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {}
+    }
+  end
 }
-
