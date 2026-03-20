cd67adc71b79b9b05f2eaf5b7117a6e819211d77
Tim Heuett <tim.heuett@gmail.com>
2026-03-19 22:59:14 -0700

    nvim: order themes roughly by brightness

--- packages/base/nvim.config/lua/plugins/themes.lua
+++ packages/base/nvim.config/lua/plugins/themes.lua
@@ -31,52 +31,52 @@ return {
     config = function()
       require("themery").setup({
         themes = {
-          -- default themes
-          "blue",
-          "darkblue",
-          "default",
-          "delek",
-          "desert",
-          "elflord",
-          "evening",
-          "habamax",
-          "industry",
-          "koehler",
-          "lunaperche",
-          "morning",
-          "murphy",
-          "pablo",
-          "peachpuff",
-          "quiet",
-          "retrobox",
-          "ron",
-          "shine",
-          "slate",
-          "sorbet",
-          "torte",
-          "vim",
-          "wildcharm",
-          "zaibatsu",
-          "zellner",
-          -- gosukiwi/vim-atom-dark
+          -- dark: builtin
+          "blue", "darkblue", "default", "desert", "elflord", "evening", "habamax", "industry",
+          "koehler", "lunaperche", "murphy", "pablo", "ron", "slate", "sorbet", "torte", "vim",
+          "wildcharm", "zaibatsu",
+          -- dark: gosukiwi/vim-atom-dark
           "atom-dark", "atom-dark-256",
-          -- rmehri01/onenord.nvim
-          "onenord", "onenord-light",
-          -- EdenEast/nightfox.nvim
-          "carbonfox", "dawnfox", "dayfox", "duskfox", "nightfox", "nordfox", "terafox",
-          -- olimorris/onedarkpro.nvim
-          "onedark", "onedark_dark", "onedark_vivid", "onelight",
-          -- folke/tokyonight.nvim
-          "tokyonight", "tokyonight-day", "tokyonight-moon", "tokyonight-night", "tokyonight-storm",
-          -- catppuccin/nvim
-          "catppuccin", "catppuccin-frappe", "catppuccin-latte", "catppuccin-macchiato", "catppuccin-mocha",
-          -- rebelot/kanagawa.nvim
-          "kanagawa", "kanagawa-dragon", "kanagawa-lotus", "kanagawa-wave",
+          -- medium: rmehri01/onenord.nvim
+          "onenord",
+          -- dark: EdenEast/nightfox.nvim
+          "carbonfox", "duskfox",
+          -- dark: olimorris/onedarkpro.nvim
+          "onedark_dark",
+          -- dark: folke/tokyonight.nvim
+          "tokyonight",
+          -- dark: catppuccin/nvim
+          "catppuccin", "catppuccin-frappe", "catppuccin-macchiato", "catppuccin-mocha",
+          -- dark: rebelot/kanagawa.nvim
+          "kanagawa", "kanagawa-dragon", "kanagawa-wave",
           -- Mofiqul/vscode.nvim
           "vscode",
           -- projekt0n/github-nvim-theme
-          "github_dark", "github_dark_dimmed", "github_dark_default", "github_dark_high_contrast", "github_dark_colorblind", "github_dark_tritanopia",
-          "github_light", "github_light_default", "github_light_high_contrast", "github_light_colorblind", "github_light_tritanopia",
+          "github_dark", "github_dark_dimmed", "github_dark_default", "github_dark_high_contrast",
+          "github_dark_colorblind", "github_dark_tritanopia",
+          -- medium: EdenEast/nightfox.nvim
+          "nightfox", "nordfox", "terafox",
+          -- medium: olimorris/onedarkpro.nvim
+          "onedark", "onedark_vivid",
+          -- medium: folke/tokyonight.nvim
+          "tokyonight-moon", "tokyonight-night", "tokyonight-storm",
+          -- light: builtin
+          "delek", "morning", "peachpuff", "quiet", "retrobox", "shine", "zellner",
+          -- light: olimorris/onedarkpro.nvim
+          "onelight",
+          -- light: folke/tokyonight.nvim
+          "tokyonight-day",
+          -- light: catppuccin/nvim
+          "catppuccin-latte",
+          -- light: rebelot/kanagawa.nvim
+          "kanagawa-lotus",
+          -- light: projekt0n/github-nvim-theme
+          "github_light", "github_light_default", "github_light_high_contrast", "github_light_colorblind",
+          "github_light_tritanopia",
+          -- light: rmehri01/onenord.nvim
+          "onenord-light",
+          -- light: EdenEast/nightfox.nvim
+          "dawnfox", "dayfox",
         },
         livePreview = true,
       })
