27817f477ed7adbb416e8e7a6290696cf43a975b
Tim Heuett <tim.heuett@gmail.com>
2026-03-31 11:43:45 -0700

    nvim: fix vim-dispatch lazy loading

--- packages/base/nvim.config/lua/plugins/dispatch.lua
+++ packages/base/nvim.config/lua/plugins/dispatch.lua
@@ -2,8 +2,9 @@ return {
   {
     "tpope/vim-dispatch",
     enabled = true,
+    cmd = "Dispatch",
     keys = {
-      { "<leader>M", "<cmd>:Make<cr>", desc = "Dispatch: run make" },
+      { "<leader>M", "<cmd>:Make<cr>",      desc = "Dispatch: run make" },
       { "<leader>T", "<cmd>:Make test<cr>", desc = "Dispatch: run make test" },
     },
     config = function()
