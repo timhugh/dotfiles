ef1e5f7075fdb6560df779d539c425b2f2265e61
Tim Heuett <tim.heuett@gmail.com>
2026-07-15 23:11:19 -0700

    darker dark themes

--- packages/base/nvim.config/lua/plugins/themes.lua
+++ packages/base/nvim.config/lua/plugins/themes.lua
@@ -17,7 +17,7 @@ return {
       update_interval = 1000,
       set_dark_mode = function()
         vim.opt.background = "dark"
-        vim.cmd("colorscheme carbonfox")
+        vim.cmd("colorscheme onedark_dark")
       end,
       set_light_mode = function()
         vim.opt.background = "light"
