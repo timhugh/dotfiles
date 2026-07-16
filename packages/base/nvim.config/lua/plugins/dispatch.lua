6438b215b18c2109438bf62eea81849493d1b14b
Tim Heuett <tim.heuett@gmail.com>
2026-07-15 23:15:28 -0700

    nvim: add bind for dispatch run

--- packages/base/nvim.config/lua/plugins/dispatch.lua
+++ packages/base/nvim.config/lua/plugins/dispatch.lua
@@ -4,8 +4,9 @@ return {
     enabled = true,
     cmd = "Dispatch",
     keys = {
-      { "<leader>M", "<cmd>:Make<cr>",      desc = "Dispatch: run make" },
-      { "<leader>T", "<cmd>:Make test<cr>", desc = "Dispatch: run make test" },
+      { "<leader>M", "<cmd>:Make<cr>",      desc = "Dispatch: make" },
+      { "<leader>T", "<cmd>:Make test<cr>", desc = "Dispatch: test" },
+      { "<leader>R", "<cmd>:Make run<cr>",  desc = "Dispatch: run" },
     },
     config = function()
     end,
