44c8f2463ce4524679ac64d39e9ebb33a14eb7f3
Tim Heuett <tim.heuett@gmail.com>
2026-08-12 20:22:07 -0700

    nvim: fix gdscript lsp

--- packages/base/nvim.config/lua/plugins/lsp.lua
+++ packages/base/nvim.config/lua/plugins/lsp.lua
@@ -60,7 +60,26 @@ return {
 
       require("mason-lspconfig").setup(opts)
 
-      vim.lsp.enable('gdscript')
+      local port = os.getenv 'GDScript_Port' or '6005'
+      local cmd = vim.lsp.rpc.connect('127.0.0.1', tonumber(port))
+
+      local server_pipe_path = vim.fn.getcwd() .. '/server.pipe'
+
+      vim.lsp.config('gdscript', {
+        enabled = true,
+        cmd = cmd,
+        filetypes = { 'gdscript' },
+        workspace_required = true,
+        root_markers = { 'project.godot' },
+        on_attach = function()
+          local is_server_running = vim.uv.fs_stat(server_pipe_path)
+          if not is_server_running then
+            vim.fn.serverstart(server_pipe_path)
+          end
+        end,
+      })
+      vim.lsp.enable('gdscript');
+
       vim.lsp.config('kotlin_lsp', {
         cmd = { "kotlin-lsp", "--stdio" },
         filetypes = { "kotlin" },
