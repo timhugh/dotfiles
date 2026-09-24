b0dda6468a6970a769986790d7c7d95c99e80467
Tim Heuett <tim.heuett@gmail.com>
2026-09-23 18:08:42 -0700

    nvim auto dark mode

--- packages/base/nvim.config/plugin/themes.lua
+++ packages/base/nvim.config/plugin/themes.lua
@@ -8,4 +8,20 @@ vim.pack.add({
   'https://github.com/projekt0n/github-nvim-theme',
 })
 
-vim.cmd('colorscheme github_light')
+require('lazyload').on_vim_enter(function()
+  vim.pack.add({
+    'https://github.com/f-person/auto-dark-mode.nvim',
+  })
+
+  require('auto-dark-mode').setup({
+    update_interval = 1000,
+    set_dark_mode = function()
+      vim.opt.background = "dark"
+      vim.cmd("colorscheme carbonfox")
+    end,
+    set_light_mode = function()
+      vim.opt.background = "light"
+      vim.cmd("colorscheme onelight")
+    end,
+  })
+end)
